73c3a9c59e1f159adef8b1dee738163109718d10
luke <gseonggyu968@gmail.com>
2024-02-11 15:54:27 +0900

    docs: 회원가입 api 문서 작성

--- be/src/main/java/yeonba/be/user/controller/UserController.java
+++ be/src/main/java/yeonba/be/user/controller/UserController.java
@@ -3,7 +3,11 @@ package yeonba.be.user.controller;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import yeonba.be.user.dto.request.UserJoinRequest;
+import yeonba.be.user.dto.response.UserJoinResponse;
 import yeonba.be.util.CustomResponse;
 
 @Tag(name = "User", description = "사용자 API")
@@ -16,5 +20,17 @@ public class UserController {
         return ResponseEntity
                 .ok()
                 .body(new CustomResponse<>());
+    }
+
+    @PostMapping("/user/join")
+    public ResponseEntity<CustomResponse<UserJoinResponse>> join(
+            @RequestBody UserJoinRequest request) {
+
+        // TODO: 회원가입 로직 구현
+        String createdJwt = "created";
+
+        return ResponseEntity
+                .ok()
+                .body(new CustomResponse<>(new UserJoinResponse(createdJwt)));
     }
 }
---
+++ be/src/main/java/yeonba/be/user/dto/response/UserJoinResponse.java
@@ -0,0 +1,9 @@
+package yeonba.be.user.dto.response;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class UserJoinResponse {
+
+    private String jwt;
+}
