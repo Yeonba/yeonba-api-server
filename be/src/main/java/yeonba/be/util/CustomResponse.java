1e568078c6bd0d3939a5eb3a2b42f0a6096e9dd2
Minjae-An <mj3242@naver.com>
2024-02-18 17:18:29 +0900

    docs: 공통 응답 DTO, 기본 응답 형태 정의(#1)

--- be/src/main/java/yeonba/be/util/CustomResponse.java
+++ be/src/main/java/yeonba/be/util/CustomResponse.java
@@ -1,12 +1,26 @@
 package yeonba.be.util;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
 public class CustomResponse<T> {
-
+    @Schema(
+        type = "string",
+        description = "요청 처리 상태",
+        example = "success"
+    )
     private final String status;
+    @Schema(
+        type = "string",
+        description = "처리 상황을 설명하는 메시지",
+        example = "정상 처리 되었습니다."
+    )
     private final String message;
+    @Schema(
+        description = "응답 데이터",
+        example = "null"
+    )
     private final T data;
 
     public CustomResponse(String message) {
