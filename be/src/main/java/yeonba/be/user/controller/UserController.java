8e845394dc141acf5cbbecbc1c71d75e9335b24a
Minjae-An <mj3242@naver.com>
2024-02-18 16:25:57 +0900

    docs: access token 재발급 API 명세서 작성(#1)

--- be/src/main/java/yeonba/be/user/controller/UserController.java
+++ be/src/main/java/yeonba/be/user/controller/UserController.java
@@ -15,93 +15,109 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import yeonba.be.user.dto.request.UserDormantRequest;
 import yeonba.be.user.dto.request.UserJoinRequest;
+import yeonba.be.user.dto.request.UserRefreshTokenRequest;
 import yeonba.be.user.dto.response.BlockedUserResponse;
 import yeonba.be.user.dto.response.BlockedUsersResponse;
 import yeonba.be.user.dto.response.UnwantedAcquaintanceResponse;
 import yeonba.be.user.dto.response.UnwantedAcquaintancesResponse;
 import yeonba.be.user.dto.response.UserJoinResponse;
+import yeonba.be.user.dto.response.UserRefreshTokenResponse;
 import yeonba.be.util.CustomResponse;
 
 @Tag(name = "User", description = "사용자 API")
 @RestController
 public class UserController {
 
-    @GetMapping("/users")
-    public ResponseEntity<CustomResponse<Void>> test() {
+  @GetMapping("/users")
+  public ResponseEntity<CustomResponse<Void>> test() {
 
-        return ResponseEntity
-            .ok()
-            .body(new CustomResponse<>());
-    }
+    return ResponseEntity
+        .ok()
+        .body(new CustomResponse<>());
+  }
 
-    @Operation(summary = "회원가입", description = "회원가입을 할 수 있습니다.")
-    @PostMapping("/users/join")
-    public ResponseEntity<CustomResponse<UserJoinResponse>> join(
-        @RequestBody UserJoinRequest request) {
+  @Operation(summary = "회원가입", description = "회원가입을 할 수 있습니다.")
+  @PostMapping("/users/join")
+  public ResponseEntity<CustomResponse<UserJoinResponse>> join(
+      @RequestBody UserJoinRequest request) {
 
-        // TODO: 회원가입 로직 구현
-        String createdJwt = "created";
+    // TODO: 회원가입 로직 구현
+    String createdJwt = "created";
 
-        return ResponseEntity
-            .ok()
-            .body(new CustomResponse<>(new UserJoinResponse(createdJwt)));
-    }
+    return ResponseEntity
+        .ok()
+        .body(new CustomResponse<>(new UserJoinResponse(createdJwt)));
+  }
 
-    @Operation(summary = "만나고 싶지 않은 지인 목록 조회", description = "만나고 싶지 않은 지인 목록을 조회합니다.")
-    @GetMapping("/users/unwanted-acquaintances")
-    public ResponseEntity<CustomResponse<UnwantedAcquaintancesResponse>> getUnwantedAcquaintances() {
+  @Operation(summary = "만나고 싶지 않은 지인 목록 조회", description = "만나고 싶지 않은 지인 목록을 조회합니다.")
+  @GetMapping("/users/unwanted-acquaintances")
+  public ResponseEntity<CustomResponse<UnwantedAcquaintancesResponse>> getUnwantedAcquaintances() {
 
-        List<UnwantedAcquaintanceResponse> sampleUnwantedAcquaintances = Arrays.asList(
-            new UnwantedAcquaintanceResponse("01012345678", "안민재"),
-            new UnwantedAcquaintanceResponse("01087654321", "김민재")
-        );
+    List<UnwantedAcquaintanceResponse> sampleUnwantedAcquaintances = Arrays.asList(
+        new UnwantedAcquaintanceResponse("01012345678", "안민재"),
+        new UnwantedAcquaintanceResponse("01087654321", "김민재")
+    );
 
-        return ResponseEntity
-            .ok()
-            .body(new CustomResponse<>(new UnwantedAcquaintancesResponse(sampleUnwantedAcquaintances)));
-    }
+    return ResponseEntity
+        .ok()
+        .body(new CustomResponse<>(new UnwantedAcquaintancesResponse(sampleUnwantedAcquaintances)));
+  }
 
-    @Operation(summary = "차단 목록 조회", description = "차단한 사용자 목록을 조회합니다.")
-    @GetMapping("/users/block")
-    public ResponseEntity<CustomResponse<BlockedUsersResponse>> getBlockedUsers() {
+  @Operation(summary = "차단 목록 조회", description = "차단한 사용자 목록을 조회합니다.")
+  @GetMapping("/users/block")
+  public ResponseEntity<CustomResponse<BlockedUsersResponse>> getBlockedUsers() {
 
-        List<BlockedUserResponse> sampleResponse = Arrays.asList(
-            new BlockedUserResponse(1, "https://avatars.githubusercontent.com/u/101340860?v=4", "안민재"),
-            new BlockedUserResponse(2, "https://avatars.githubusercontent.com/u/101340860?v=4", "안민재")
-        );
+    List<BlockedUserResponse> sampleResponse = Arrays.asList(
+        new BlockedUserResponse(1, "https://avatars.githubusercontent.com/u/101340860?v=4", "안민재"),
+        new BlockedUserResponse(2, "https://avatars.githubusercontent.com/u/101340860?v=4", "안민재")
+    );
 
-        return ResponseEntity
-            .ok()
-            .body(new CustomResponse<>(new BlockedUsersResponse(sampleResponse)));
-    }
+    return ResponseEntity
+        .ok()
+        .body(new CustomResponse<>(new BlockedUsersResponse(sampleResponse)));
+  }
 
-    @Operation(summary = "차단 해제", description = "차단한 사용자를 해제할 수 있습니다.")
-    @DeleteMapping("/users/{userId}/block")
-    public ResponseEntity<CustomResponse<Void>> unblockUser(
-        @Parameter(description = "사용자 ID", example = "1")
-        @PathVariable long userId) {
+  @Operation(summary = "차단 해제", description = "차단한 사용자를 해제할 수 있습니다.")
+  @DeleteMapping("/users/{userId}/block")
+  public ResponseEntity<CustomResponse<Void>> unblockUser(
+      @Parameter(description = "사용자 ID", example = "1")
+      @PathVariable long userId) {
 
-        return ResponseEntity
-            .ok()
-            .body(new CustomResponse<>());
-    }
+    return ResponseEntity
+        .ok()
+        .body(new CustomResponse<>());
+  }
 
-    @Operation(summary = "휴면 계정 전환", description = "계정의 휴면 상태를 전환할 수 있습니다.")
-    @PatchMapping("/users/dormant")
-    public ResponseEntity<CustomResponse<Void>> dormantUser(
-        @RequestBody UserDormantRequest request) {
+  @Operation(summary = "휴면 계정 전환", description = "계정의 휴면 상태를 전환할 수 있습니다.")
+  @PatchMapping("/users/dormant")
+  public ResponseEntity<CustomResponse<Void>> dormantUser(
+      @RequestBody UserDormantRequest request) {
 
-        return ResponseEntity
-            .ok()
-            .body(new CustomResponse<>());
-    }
+    return ResponseEntity
+        .ok()
+        .body(new CustomResponse<>());
+  }
 
-    @Operation(summary = "회원 탈퇴", description = "회원 탈퇴를 할 수 있습니다.")
-    @DeleteMapping("/users")
-    public ResponseEntity<CustomResponse<Void>> deleteUser() {
+  @Operation(summary = "회원 탈퇴", description = "회원 탈퇴를 할 수 있습니다.")
+  @DeleteMapping("/users")
+  public ResponseEntity<CustomResponse<Void>> deleteUser() {
 
-        return ResponseEntity
-            .ok()
-            .body(new CustomResponse<>());
-    }
+    return ResponseEntity
+        .ok()
+        .body(new CustomResponse<>());
+  }
+
+  @Operation(
+      summary = "access token 재발급",
+      description = "refresh token을 검증하고, access token을 재발급합니다."
+  )
+  @PostMapping("/users/refresh")
+  public ResponseEntity<CustomResponse<UserRefreshTokenResponse>> refresh(
+      @RequestBody UserRefreshTokenRequest request) {
+    String createdJwt = "created";
+
+    return ResponseEntity
+        .ok()
+        .body(new CustomResponse<>(new UserRefreshTokenResponse(createdJwt)));
+  }
 }
---
+++ be/src/main/java/yeonba/be/user/dto/response/UserRefreshTokenResponse.java
@@ -0,0 +1,19 @@
+package yeonba.be.user.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserRefreshTokenResponse {
+	@Schema(
+		type = "string",
+		example = """
+			eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9
+			.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ
+			.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c""",
+		description = "새로 발급된 access token"
+	)
+	private String accessToken;
+}
