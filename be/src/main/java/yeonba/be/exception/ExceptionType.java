86050e8dc4f60b122c510f6c6f396da21235c41b
Minjae-An <mj3242@naver.com>
2024-03-06 17:00:02 +0900

    feat: 서비스 커스텀 예외 enum 정의(#9)
    - HttpStatus, 서비스 고유 예외 코드(code), 예외 사유를 필드로 가짐
    - 예시 커스텀 예외 규정

---
+++ be/src/main/java/yeonba/be/exception/ExceptionType.java
@@ -0,0 +1,30 @@
+package yeonba.be.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum ExceptionType {
+  INTERNAL_SERVER_ERROR(
+      HttpStatus.INTERNAL_SERVER_ERROR,
+      "COMMON500",
+      "서버 에러 관리자에게 문의 바랍니다."
+  ),
+
+  // 서비스 고유 예외 코드 형식 : {관련 엔티티 이름(대문자)}{HTTP 응답 코드}{고유 번호}
+  USER_NOT_FOUND(
+      HttpStatus.BAD_REQUEST,
+      "USER4001",
+      "해당 사용자가 존재하지 않습니다."
+  );
+
+  private final HttpStatus httpStatus;
+  private final String code;
+  private final String reason;
+
+  ExceptionType(HttpStatus httpStatus, String code, String reason) {
+    this.httpStatus = httpStatus;
+    this.code = code;
+    this.reason = reason;
+  }
+}
