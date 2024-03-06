f890d0ee8f0ffc1fa1f883ed745334669f511c24
Minjae-An <mj3242@naver.com>
2024-03-06 21:56:25 +0900

    feat: JpaAuditing 활용을 위한 설정 정의(#14)

---
+++ be/src/main/java/yeonba/be/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package yeonba.be.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
