5582c34fb29bb625fcb72ecd7cda985bf9f7c90b
Minjae-An <mj3242@naver.com>
2024-03-06 17:46:10 +0900

    feat: 커스텀 예외 응답을 위한 생성 로직 추가(#9)

--- be/src/main/java/yeonba/be/util/CustomResponse.java
+++ be/src/main/java/yeonba/be/util/CustomResponse.java
@@ -4,27 +4,38 @@ import lombok.Getter;
 
 @Getter
 public class CustomResponse<T> {
-    private final String status;
 
-    private final String message;
+  private final String status;
 
-    private final T data;
+  private final String message;
 
-    public CustomResponse(String message) {
-        this.status = "fail";
-        this.message = message;
-        this.data = null;
-    }
+  private final T data;
 
-    public CustomResponse(T data) {
-        this.status = "success";
-        this.message = null;
-        this.data = data;
-    }
+  public CustomResponse(String message) {
+    this.status = "fail";
+    this.message = message;
+    this.data = null;
+  }
 
-    public CustomResponse() {
-        this.status = "success";
-        this.message = null;
-        this.data = null;
-    }
+  public CustomResponse(T data) {
+    this.status = "success";
+    this.message = null;
+    this.data = data;
+  }
+
+  public CustomResponse() {
+    this.status = "success";
+    this.message = null;
+    this.data = null;
+  }
+
+  private CustomResponse(String message, T data) {
+    this.status = "fail";
+    this.message = message;
+    this.data = data;
+  }
+
+  public static <T> CustomResponse<T> onFailure(String message, T data) {
+    return new CustomResponse<>(message, data);
+  }
 }
