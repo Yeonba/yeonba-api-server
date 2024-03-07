5f025254eb4f5c80cd9867fb501137c61a5c1084
Minjae-An <mj3242@naver.com>
2024-03-07 20:52:55 +0900

    refactor: 코드 정렬 수정(#14)

--- be/src/main/java/yeonba/be/arrow/repository/ArrowTransactionCommandRepository.java
+++ be/src/main/java/yeonba/be/arrow/repository/ArrowTransactionCommandRepository.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Repository;
 import yeonba.be.arrow.entity.ArrowTransaction;
 
 @Repository
-public interface ArrowTransactionCommandRepository
-    extends JpaRepository<ArrowTransaction, Long> {
+public interface ArrowTransactionCommandRepository extends JpaRepository<ArrowTransaction, Long> {
+
 }
