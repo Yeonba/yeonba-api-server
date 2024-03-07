24a4762bdec9b5de2ca0b5dc449c0e6c5469ce0c
Minjae-An <mj3242@naver.com>
2024-03-07 15:14:47 +0900

    refactor: 불필요한 code 필드 삭제(#9)

--- be/src/main/java/yeonba/be/exception/ExceptionType.java
+++ be/src/main/java/yeonba/be/exception/ExceptionType.java
@@ -8,27 +8,21 @@ public enum ExceptionType {
 
   BAD_REQUEST(
       HttpStatus.BAD_REQUEST,
-      "COMMON400",
       "잘못된 요청입니다."),
 
   INTERNAL_SERVER_ERROR(
       HttpStatus.INTERNAL_SERVER_ERROR,
-      "COMMON500",
       "서버 에러 관리자에게 문의 바랍니다."),
 
-  // 서비스 고유 예외 코드 형식 : {관련 엔티티 이름(대문자)}{HTTP 응답 코드}{고유 번호}
   USER_NOT_FOUND(
       HttpStatus.BAD_REQUEST,
-      "USER4001",
       "해당 사용자가 존재하지 않습니다.");
 
   private final HttpStatus httpStatus;
-  private final String code;
   private final String reason;
 
-  ExceptionType(HttpStatus httpStatus, String code, String reason) {
+  ExceptionType(HttpStatus httpStatus, String reason) {
     this.httpStatus = httpStatus;
-    this.code = code;
     this.reason = reason;
   }
 }
