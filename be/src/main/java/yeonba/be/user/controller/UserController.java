99de618b5a486d8c1be26ebc243459229bc00453
Minjae-An <mj3242@naver.com>
2024-03-08 19:15:00 +0900

    feat: 차단하기 API 구현(#22)

--- be/src/main/java/yeonba/be/user/controller/UserController.java
+++ be/src/main/java/yeonba/be/user/controller/UserController.java
@@ -4,23 +4,29 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import yeonba.be.user.dto.request.UserQueryRequest;
 import yeonba.be.user.dto.request.UserReportRequest;
 import yeonba.be.user.dto.response.UserProfileResponse;
 import yeonba.be.user.dto.response.UserQueryPageResponse;
+import yeonba.be.user.service.BlockService;
 import yeonba.be.util.CustomResponse;
 
 @Tag(name = "User", description = "사용자 API")
 @RestController
+@RequiredArgsConstructor
 public class UserController {
+
+  private final BlockService blockService;
 
   @Operation(
       summary = "이성(다른 사용자) 목록 조회",
@@ -124,18 +130,15 @@ public class UserController {
         .body(new CustomResponse<>());
   }
 
-  @Operation(
-      summary = "차단하기",
-      description = "다른 사용자를 차단할 수 있습니다."
-  )
-  @ApiResponse(
-      responseCode = "204",
-      description = "차단 요청 정상 처리"
-  )
+  @Operation(summary = "차단하기", description = "다른 사용자를 차단할 수 있습니다.")
+  @ApiResponse(responseCode = "202", description = "차단 요청 정상 처리")
   @PostMapping("/users/{userId}/block")
   public ResponseEntity<CustomResponse<Void>> block(
+      @RequestAttribute("userId") long userId,
       @Parameter(description = "차단하는 사용자 ID", example = "1")
-      @PathVariable long userId) {
+      @PathVariable("userId") long blockedUserId) {
+
+    blockService.block(userId, blockedUserId);
 
     return ResponseEntity
         .accepted()
