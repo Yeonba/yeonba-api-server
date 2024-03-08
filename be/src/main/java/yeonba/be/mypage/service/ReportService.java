d884dc5494452c6ee7f8a77766a911f85f22d78d
Minjae-An <mj3242@naver.com>
2024-03-08 16:39:33 +0900

    feat: 신고하기 비즈니스 로직 구성(#19)
    신고하기는 다음 과정을 거쳐 이뤄진다.
    
    1. 신고 요청 검증
    - 카테고리가 기타가 아닐 경우 사유는 null이므로 검증하지 않음
    - 카테고리가 기타인데 사유가 존재하지 않는 경우 예외 발생
    - 사유 존재시 길이가 최대 길이(1024자)를 초과할 경우 예외 발생
    
    2. 자기 자신을 신고하는 상황 검증
    3. 신고한 유저, 신고 당한 유저 조회
    4. 신고 엔티티 생성 및 저장

---
+++ be/src/main/java/yeonba/be/mypage/service/ReportService.java
@@ -0,0 +1,87 @@
+package yeonba.be.mypage.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
+import yeonba.be.exception.ExceptionType;
+import yeonba.be.exception.GeneralException;
+import yeonba.be.mypage.entity.Report;
+import yeonba.be.mypage.repository.ReportCommandRepository;
+import yeonba.be.user.dto.request.UserReportRequest;
+import yeonba.be.user.entity.User;
+import yeonba.be.user.service.UserService;
+
+@Service
+@RequiredArgsConstructor
+public class ReportService {
+
+  private final String CATEGORY_ETC = "기타";
+  private final int MAX_REPORT_REASON_LENGTH = 1024;
+
+  private final UserService userService;
+  private final ReportCommandRepository reportCommandRepository;
+
+  /*
+    신고 생성은 다음 과정을 거쳐 이뤄진다.
+    1. 신고 요청 검증
+    2. 자기 자신을 신고할 수 없는 상황 검증
+    3. 신고한 유저, 신고 당한 유저 조회
+    4. 신고 엔티티 생성 및 저장
+   */
+  @Transactional
+  public void makeReport(
+      long userId,
+      long reportedUserId,
+      UserReportRequest request) {
+
+    checkReportRequest(request);
+    if (userId == reportedUserId) {
+      throw new GeneralException(ExceptionType.CAN_NOT_REPORT_SELF);
+    }
+
+    User user = userService.findById(userId);
+    User reportedUser = userService.findById(reportedUserId);
+
+    Report report = new Report(
+        user,
+        reportedUser,
+        request.getCategory(),
+        request.getReason());
+    reportCommandRepository.save(report);
+  }
+
+  /*
+    신고 요청에 포함된 카테고리와 사유를 검증한다.
+    - 카테고리가 기타가 아닐 경우 사유는 null이므로 검증하지 않는다.
+    - 카테고리가 기타인데 사유가 존재하지 않는 경우 예외 발생
+    - 카테고리가 기타인데 사유 길이가 1024자를 초과할 경우 예외 발생
+   */
+  private void checkReportRequest(UserReportRequest request) {
+
+    String category = request.getCategory();
+    String reason = request.getReason();
+
+    if (!category.equals(CATEGORY_ETC)) {
+      return;
+    }
+
+    if (!isReportReasonExist(reason)) {
+      throw new GeneralException(ExceptionType.REPORT_REASON_NOT_EXIST);
+    }
+
+    if (!isReportReasonLessOrEqualThanMaxLength(reason)) {
+      throw new GeneralException(ExceptionType.REPORT_REASON_LENGTH_NOT_VALID);
+    }
+  }
+
+  private boolean isReportReasonExist(String reason) {
+
+    return StringUtils.hasText(reason);
+  }
+
+  private boolean isReportReasonLessOrEqualThanMaxLength(String reason) {
+
+    return reason.length() <= MAX_REPORT_REASON_LENGTH;
+  }
+}
