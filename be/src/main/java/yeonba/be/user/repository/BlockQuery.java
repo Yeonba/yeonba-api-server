10ef6154b9c93b6eba4517149706b38a02a43d8f
Minjae-An <mj3242@naver.com>
2024-03-09 20:27:58 +0900

    feat: 차단 내역 존재 확인 로직 구현(#22)
    차단한 사용자, 차단 당한 사용자 기반 차단 내역이 존재하는 지
    확인하는 로직 구현

---
+++ be/src/main/java/yeonba/be/user/repository/BlockQuery.java
@@ -0,0 +1,17 @@
+package yeonba.be.user.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import yeonba.be.user.entity.User;
+
+@Component
+@RequiredArgsConstructor
+public class BlockQuery {
+
+  private final BlockRepository blockRepository;
+
+  public boolean existsByUserAndBlockedUser(User user, User blockedUser) {
+
+    return blockRepository.existsByUserAndBlockedUser(user, blockedUser);
+  }
+}
