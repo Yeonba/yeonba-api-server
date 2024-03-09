57c568a455a9e5bc2684e05103e3cd7ef69faff9
Minjae-An <mj3242@naver.com>
2024-03-09 20:29:09 +0900

    feat: 차단 연관 예외 enum 정의(#22)
    - 자기 자신을 차단하는 경우
    - 이미 차단한 사용자인 경우

--- be/src/main/java/yeonba/be/exception/ExceptionType.java
+++ be/src/main/java/yeonba/be/exception/ExceptionType.java
@@ -16,7 +16,16 @@ public enum ExceptionType {
 
   USER_NOT_FOUND(
       HttpStatus.BAD_REQUEST,
-      "해당 사용자가 존재하지 않습니다.");
+      "해당 사용자가 존재하지 않습니다."),
+
+  // block
+  CAN_NOT_BLOCK_SELF(
+      HttpStatus.BAD_REQUEST,
+      "자기 자신을 차단할 수 없습니다."),
+
+  ALREADY_BLOCKED_USER(
+      HttpStatus.BAD_REQUEST,
+      "이미 차단한 사용자입니다.");
 
   private final HttpStatus httpStatus;
   private final String reason;
