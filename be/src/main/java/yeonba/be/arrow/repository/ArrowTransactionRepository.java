9b490938f7a9ebb22dea55136d6891231fc39596
Minjae-An <mj3242@naver.com>
2024-03-09 17:18:29 +0900

    feat: 확살 송수신 내역 repository, 내역 확인 로직 추가(#24)
    - 화살 송수신 내역 repository 정의
    - 보낸 사용자, 받은 사용자를 통해 내역 존재 여부를 확인하는 로직 구현

---
+++ be/src/main/java/yeonba/be/arrow/repository/ArrowTransactionRepository.java
@@ -0,0 +1,12 @@
+package yeonba.be.arrow.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import yeonba.be.arrow.entity.ArrowTransaction;
+import yeonba.be.user.entity.User;
+
+@Repository
+public interface ArrowTransactionRepository extends JpaRepository<ArrowTransaction, Long> {
+
+  boolean existsBySentUserAndReceivedUser(User sentUser, User receivedUser);
+}
