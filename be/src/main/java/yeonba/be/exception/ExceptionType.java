d781de37c3c4fea5d05a1ce7ac2e71beb2de7976
Minjae-An <mj3242@naver.com>
2024-03-09 17:25:05 +0900

    feat: 화살 관련 예외 enum 정의(#24)
    다음 상황들에 대한 예외 enum을 정의하였다.
    - 자기 자신에게 화살을 보내는 경우
    - 이미 화살을 보낸 사용자인 경우
    - 가진 화살 수가 부족해 화살을 보낼 수 없는 경우

--- be/src/main/java/yeonba/be/exception/ExceptionType.java
+++ be/src/main/java/yeonba/be/exception/ExceptionType.java
@@ -16,7 +16,20 @@ public enum ExceptionType {
 
   USER_NOT_FOUND(
       HttpStatus.BAD_REQUEST,
-      "해당 사용자가 존재하지 않습니다.");
+      "해당 사용자가 존재하지 않습니다."),
+
+  // arrow
+  CAN_NOT_SEND_ARROW_SELF(
+      HttpStatus.BAD_REQUEST,
+      "자기 자신에게 화살을 보낼 수 없습니다."),
+
+  ALREADY_SENT_ARROW_USER(
+      HttpStatus.BAD_REQUEST,
+      "이미 화살을 보낸 사용자입니다."),
+
+  NOT_ENOUGH_ARROW_TO_SEND(
+      HttpStatus.BAD_REQUEST,
+      "가진 화살 수가 부족해 화살을 보낼 수 없습니다.");
 
   private final HttpStatus httpStatus;
   private final String reason;
