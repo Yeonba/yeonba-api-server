62c45888e73819d9fb17a2f2de283f3a12c44711
Minjae-An <mj3242@naver.com>
2024-03-09 21:30:02 +0900

    feat: 즐겨찾기 저장 기능 구현(#26)
    즐겨찾기 엔티티 저장 로직 구현

---
+++ be/src/main/java/yeonba/be/user/repository/favorite/FavoriteCommand.java
@@ -0,0 +1,18 @@
+package yeonba.be.user.repository.favorite;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import yeonba.be.user.entity.Favorite;
+
+@Component
+@RequiredArgsConstructor
+public class FavoriteCommand {
+
+  private final FavoriteRepository favoriteRepository;
+
+  public Favorite save(Favorite favorite){
+
+    return favoriteRepository.save(favorite);
+  }
+
+}
