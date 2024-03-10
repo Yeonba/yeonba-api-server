b5fde73efaaeb8f6fc5e31dce97db596639f47bc
Minjae-An <mj3242@naver.com>
2024-03-10 17:29:53 +0900

    refactor: 사용자 query가 조회 기능 전담하도록 일괄 수정(#18)

--- be/src/main/java/yeonba/be/user/service/UserService.java
+++ be/src/main/java/yeonba/be/user/service/UserService.java
@@ -2,7 +2,6 @@ package yeonba.be.user.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import yeonba.be.user.entity.User;
 import yeonba.be.user.repository.UserQuery;
 
 @Service
@@ -11,8 +10,4 @@ public class UserService {
 
   private final UserQuery userQuery;
 
-  public User findById(long userId) {
-
-    return userQuery.findById(userId);
-  }
 }
