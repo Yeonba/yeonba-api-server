fa4204d56705542c5242d98223d754444df3fd38
Minjae-An <mj3242@naver.com>
2024-03-11 22:08:47 +0900

    feat: 상대 프로필 조회 비즈니스 로직 추가(#27)
    프로필을 조회하는 사용자 ID의 경우 상대방이 화살을 보넀던 사용자인지
    확인하기 위해 파라미터로 받는다.

--- be/src/main/java/yeonba/be/user/service/UserService.java
+++ be/src/main/java/yeonba/be/user/service/UserService.java
@@ -1,12 +1,49 @@
 package yeonba.be.user.service;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import yeonba.be.arrow.repository.ArrowQuery;
+import yeonba.be.user.dto.response.UserProfileResponse;
+import yeonba.be.user.entity.ProfilePhoto;
+import yeonba.be.user.entity.User;
 import yeonba.be.user.repository.UserQuery;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
+
+    private final ArrowQuery arrowQuery;
     private final UserQuery userQuery;
+
+
+    @Transactional(readOnly = true)
+    public UserProfileResponse getTargetUserProfile(long userId, long targetUserId) {
+
+        User user = userQuery.findById(userId);
+        User targetUser = userQuery.findById(targetUserId);
+
+        List<String> profilePhotosUrls = targetUser
+            .getProfilePhotos().stream()
+            .map(ProfilePhoto::getPhotoUrl)
+            .toList();
+
+        boolean isAlreadySentArrow = arrowQuery.isArrowTransactionExist(user, targetUser);
+
+        return new UserProfileResponse(
+            profilePhotosUrls,
+            targetUser.getNickname(),
+            targetUser.getArrow(),
+            targetUser.getAge(),
+            targetUser.getHeight(),
+            targetUser.getArea().getName(),
+            targetUser.getPhotoSyncRate(),
+            targetUser.getDrinkingHabit(),
+            targetUser.getSmokingHabit(),
+            targetUser.getVocalRange().getClassification(),
+            targetUser.getAnimal().getName(),
+            isAlreadySentArrow);
+    }
 }
