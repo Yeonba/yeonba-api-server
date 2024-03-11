14232a0edde4573fb32fd04f7ab7e3462b20b069
Minjae-An <mj3242@naver.com>
2024-03-11 14:03:45 +0900

    feat: 사용자 관련 예외 enum 정의 및 사용위치 수정(#19)
    사용자 엔티티와 관련하여 서비스에서 고유하게 발생할 수 있는 예외 enum 정의.
    기존 예외 enum 사용 위치 수정

--- be/src/main/java/yeonba/be/user/service/UserService.java
+++ be/src/main/java/yeonba/be/user/service/UserService.java
@@ -2,6 +2,8 @@ package yeonba.be.user.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import yeonba.be.exception.GeneralException;
+import yeonba.be.exception.UserException;
 import yeonba.be.user.entity.User;
 import yeonba.be.user.repository.UserRepository;
 
@@ -14,6 +16,6 @@ public class UserService {
     public User findById(long userId) {
 
         return userRepository.findById(userId)
-            .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 사용자입니다."));
+            .orElseThrow(() -> new GeneralException(UserException.USER_NOT_FOUND));
     }
 }
