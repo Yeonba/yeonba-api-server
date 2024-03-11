01f31382f573b52c3047e77c7ecc4c41094df8c0
Minjae-An <mj3242@naver.com>
2024-03-11 19:51:39 +0900

    feat: 화살 관련 예외 enum 추가(#24)
    - 이미 화살을 보낸 사용자 예외
    - 화살이 부족하여 화살을 보낼 수 없는 예외

--- be/src/main/java/yeonba/be/exception/ArrowException.java
+++ be/src/main/java/yeonba/be/exception/ArrowException.java
@@ -6,7 +6,15 @@ public enum ArrowException implements BaseException {
 
   ALREADY_CHECKED_USER(
       HttpStatus.BAD_REQUEST,
-      "이미 출석 체크한 사용자입니다.");
+      "이미 출석 체크한 사용자입니다."),
+
+  ALREADY_SENT_ARROW_USER(
+      HttpStatus.BAD_REQUEST,
+      "이미 화살을 보낸 사용자입니다."),
+
+  NOT_ENOUGH_ARROW_TO_SEND(
+      HttpStatus.BAD_REQUEST,
+      "화살이 부족하여 화살을 보낼 수 없습니다.");
 
   private final HttpStatus httpStatus;
   private final String reason;
