7efedf975f600574176389c88f2b110743efaea1
Minjae-An <mj3242@naver.com>
2024-03-11 21:41:48 +0900

    feat: 이름 칼럼, 생성자 추가(#27)
    - 이름 칼럼 추가
    - 테스트시 활용 가능한 생성자 추가

--- be/src/main/java/yeonba/be/user/entity/Animal.java
+++ be/src/main/java/yeonba/be/user/entity/Animal.java
@@ -1,25 +1,29 @@
 package yeonba.be.user.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Table(name = "animals")
 @Getter
 @Entity
-@NoArgsConstructor
-@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Animal {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
+    private String name;
 
-
+    public Animal(String name) {
+        this.name = name;
+    }
 }
