18f17347c870d946a9a28dff9f290f47d7bfc40d
Minjae-An <mj3242@naver.com>
2024-03-11 21:18:42 +0900

    feat: 존재하지 않는 즐겨찾기 예외 enum 추가(#34)

--- be/src/main/java/yeonba/be/exception/FavoriteException.java
+++ be/src/main/java/yeonba/be/exception/FavoriteException.java
@@ -6,7 +6,11 @@ public enum FavoriteException implements BaseException {
 
   ALREADY_FAVORITE_USER(
       HttpStatus.BAD_REQUEST,
-      "이미 즐겨찾기한 사용자입니다.");
+      "이미 즐겨찾기한 사용자입니다."),
+
+  FAVORITE_NOT_FOUND(
+      HttpStatus.BAD_REQUEST,
+      "해당 즐겨찾기 내역이 존재하지 않습니다.");
 
   private final HttpStatus httpStatus;
   private final String reason;
