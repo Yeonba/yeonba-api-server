1c74af888188bdf2d7d2ad4869be6915721c892a
Minjae-An <mj3242@naver.com>
2024-03-13 22:02:25 +0900

    refactor: 병합에 따른 코드 배치 수정(#34)
    dev 브랜치 작업 내역 병합에 따른 코드 배치 수정

--- be/src/main/java/yeonba/be/user/controller/UserController.java
+++ be/src/main/java/yeonba/be/user/controller/UserController.java
@@ -20,6 +20,7 @@ import yeonba.be.user.dto.request.UserReportRequest;
 import yeonba.be.user.dto.response.UserProfileResponse;
 import yeonba.be.user.dto.response.UserQueryPageResponse;
 import yeonba.be.user.service.BlockService;
+import yeonba.be.user.service.FavoriteService;
 import yeonba.be.util.CustomResponse;
 
 @Tag(name = "User", description = "사용자 API")
@@ -27,8 +28,10 @@ import yeonba.be.util.CustomResponse;
 @RequiredArgsConstructor
 public class UserController {
 
-  private final ReportService reportService;
   private final BlockService blockService;
+  private final ReportService reportService;
+  private final FavoriteService favoriteService;
+
 
   @Operation(
       summary = "이성(다른 사용자) 목록 조회",
@@ -77,36 +80,30 @@ public class UserController {
         ));
   }
 
-  @Operation(
-      summary = "즐겨찾기 등록",
-      description = "다른 사용자를 자신의 즐겨찾기에 등록할 수 있습니다."
-  )
-  @ApiResponse(
-      responseCode = "202",
-      description = "즐겨찾기 등록 정상 처리"
-  )
+  @Operation(summary = "즐겨찾기 등록", description = "다른 사용자를 자신의 즐겨찾기에 등록할 수 있습니다.")
+  @ApiResponse(responseCode = "202", description = "즐겨찾기 등록 정상 처리")
   @PostMapping("/favorites/{userId}")
   public ResponseEntity<CustomResponse<Void>> registerFavorite(
+      @RequestAttribute("userId") long userId,
       @Parameter(description = "즐겨찾기에 등록될 사용자 ID", example = "1")
-      @PathVariable long userId) {
+      @PathVariable("userId") long favoriteUserId) {
+
+    favoriteService.addFavorite(userId, favoriteUserId);
 
     return ResponseEntity
         .accepted()
         .body(new CustomResponse<>());
   }
 
-  @Operation(
-      summary = "즐겨찾기 삭제",
-      description = "즐겨찾기에 등록한 사용자를 삭제합니다."
-  )
-  @ApiResponse(
-      responseCode = "202",
-      description = "즐겨찾기 삭제 정상 처리"
-  )
+  @Operation(summary = "즐겨찾기 삭제", description = "즐겨찾기에 등록한 사용자를 삭제합니다.")
+  @ApiResponse(responseCode = "202", description = "즐겨찾기 삭제 정상 처리")
   @DeleteMapping("/favorites/{userId}")
   public ResponseEntity<CustomResponse<Void>> deleteFavorite(
+      @RequestAttribute("userId") long userId,
       @Parameter(description = "즐겨찾기에서 삭제할 사용자 ID", example = "1")
-      @PathVariable long userId) {
+      @PathVariable("userId") long favoriteUserId) {
+
+    favoriteService.deleteFavorite(userId, favoriteUserId);
 
     return ResponseEntity
         .accepted()
