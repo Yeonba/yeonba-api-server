6cc2cb659ff44e296752dbc7f6e023022de0bc16
Minjae-An <mj3242@naver.com>
2024-03-13 15:47:24 +0900

    feat: 사용자 프로필 상세 응답 DTO 수정(#27)
    - 음주 성향, 흡연 성향 필드 삭제
    - 생성자 성별 필드 설정 로직 수정
    - 코드 정렬

--- be/src/main/java/yeonba/be/mypage/dto/response/UserProfileDetailResponse.java
+++ be/src/main/java/yeonba/be/mypage/dto/response/UserProfileDetailResponse.java
@@ -2,9 +2,7 @@ package yeonba.be.mypage.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.Size;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -17,101 +15,74 @@ public class UserProfileDetailResponse {
 
     @Schema(
         type = "array",
-        description = "프로필 이미지"
-    )
-    @Size(min = 3, max = 3)
+        description = "프로필 이미지")
     private List<String> profilePhotoUrls;
 
     @Schema(
         type = "string",
         description = "성별",
-        example = "남"
-    )
+        example = "남")
     private String gender;
 
     @Schema(
         type = "string",
         description = "이름",
-        example = "안민재"
-    )
+        example = "안민재")
     private String name;
 
     @Schema(
         type = "string",
         description = "생년월일",
-        example = "1998-01-01"
-    )
+        example = "1998-01-01")
     @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate birth;
 
     @Schema(
         type = "number",
         description = "키",
-        example = "181"
-    )
+        example = "181")
     private int height;
 
     @Schema(
         type = "string",
         description = "이메일",
-        example = "mj3242@naver.com"
-    )
+        example = "mj3242@naver.com")
     private String email;
 
     @Schema(
         type = "string",
         description = "전화번호",
-        example = "01011112222"
-    )
+        example = "01011112222")
     private String phoneNumber;
 
     @Schema(
         type = "string",
         description = "별명",
-        example = "존잘남"
-    )
+        example = "존잘남")
     private String nickname;
 
     @Schema(
         type = "number",
         description = "사진 싱크로율",
-        example = "80"
-    )
-    private int photoSyncRate;
+        example = "80")
+    private double photoSyncRate;
 
     @Schema(
         type = "string",
         description = "체형",
-        example = "메른 체형"
-    )
+        example = "메른 체형")
     private String bodyType;
 
     @Schema(
         type = "string",
         description = "직업",
-        example = "학생"
-    )
+        example = "학생")
     private String job;
 
     @Schema(
         type = "string",
-        description = "음주 성향",
-        example = "자주"
-    )
-    private String drinkingHabit;
-
-    @Schema(
-        type = "string",
-        description = "흡연 성향",
-        example = "가끔"
-    )
-    private String smokingHabit;
-
-    @Schema(
-        type = "string",
         description = "MBTI",
-        example = "ISTP"
-    )
+        example = "ISTP")
     private String mbti;
 
     public UserProfileDetailResponse(User user) {
@@ -119,18 +90,16 @@ public class UserProfileDetailResponse {
         this.profilePhotoUrls = user.getProfilePhotos().stream()
             .map(ProfilePhoto::getPhotoUrl)
             .toList();
-        this.gender = user.isGender() ? "남" : "여";
+        this.gender = user.getGender();
         this.name = user.getName();
         this.birth = user.getBirth();
         this.height = user.getHeight();
         this.email = user.getEmail();
         this.phoneNumber = user.getPhoneNumber();
         this.nickname = user.getNickname();
-        this.photoSyncRate = (int) (user.getPhotoSyncRate());
+        this.photoSyncRate = user.getPhotoSyncRate();
         this.bodyType = user.getBodyType();
         this.job = user.getJob();
-        this.drinkingHabit = user.getDrinkingHabit();
-        this.smokingHabit = user.getSmokingHabit();
         this.mbti = user.getMbti();
     }
 }
