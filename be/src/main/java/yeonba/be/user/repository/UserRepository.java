0d4b90f881ee029a4fafcaaa186c3b161723b11b
Minjae-An <mj3242@naver.com>
2024-03-13 19:07:26 +0900

    feat: 이메일 기반 사용자 조회 로직 추가(#35)

--- be/src/main/java/yeonba/be/user/repository/UserRepository.java
+++ be/src/main/java/yeonba/be/user/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package yeonba.be.user.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import yeonba.be.user.entity.User;
@@ -7,4 +8,5 @@ import yeonba.be.user.entity.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+  Optional<User> findByEmail(String email);
 }
