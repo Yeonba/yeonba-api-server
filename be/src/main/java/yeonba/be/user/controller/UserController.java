45adb7a64e0a157a99df73e3cde0ba2a68c5c152
Minjae-An <mj3242@naver.com>
2024-03-14 21:24:49 +0900

    refactor: 병합에 따른 코드 재배치(#35)
    dev 브랜치 작업 내역 병합에 따른 코드 재배치

--- be/src/main/java/yeonba/be/user/controller/UserController.java
+++ be/src/main/java/yeonba/be/user/controller/UserController.java
@@ -21,6 +21,7 @@ import yeonba.be.user.dto.response.UserProfileResponse;
 import yeonba.be.user.dto.response.UserQueryPageResponse;
 import yeonba.be.user.service.BlockService;
 import yeonba.be.user.service.FavoriteService;
+import yeonba.be.user.service.UserService;
 import yeonba.be.util.CustomResponse;
 
 @Tag(name = "User", description = "사용자 API")
@@ -29,8 +30,9 @@ import yeonba.be.util.CustomResponse;
 public class UserController {
 
   private final BlockService blockService;
-  private final ReportService reportService;
   private final FavoriteService favoriteService;
+  private final ReportService reportService;
+  private final UserService userService;
 
   @Operation(
       summary = "이성(다른 사용자) 목록 조회",
@@ -50,33 +52,19 @@ public class UserController {
   }
 
 
-  @Operation(
-      summary = "다른 사용자 프로필 조회",
-      description = "다른 사용자의 프로필을 조회할 수 있습니다."
-  )
-  @ApiResponse(
-      responseCode = "200",
-      description = "사용자 프로필 정상 조회"
-  )
+  @Operation(summary = "다른 사용자 프로필 조회", description = "다른 사용자의 프로필을 조회할 수 있습니다.")
+  @ApiResponse(responseCode = "200", description = "사용자 프로필 정상 조회")
   @GetMapping("/users/{userId}")
   public ResponseEntity<CustomResponse<UserProfileResponse>> profile(
+      @RequestAttribute("userId") long userId,
       @Parameter(description = "조회대상 사용자 ID", example = "1")
-      @PathVariable long userId) {
+      @PathVariable("userId") long targetUserId) {
+
+    UserProfileResponse response = userService.getTargetUserProfile(userId, targetUserId);
 
     return ResponseEntity
         .ok()
-        .body(new CustomResponse<>(
-            new UserProfileResponse(
-                "존잘남",
-                23,
-                177,
-                "서울시 강남구",
-                80,
-                "저음",
-                "여우상",
-                false
-            )
-        ));
+        .body(new CustomResponse<>(response));
   }
 
   @Operation(summary = "즐겨찾기 등록", description = "다른 사용자를 자신의 즐겨찾기에 등록할 수 있습니다.")
--- be/src/main/java/yeonba/be/user/service/UserService.java
+++ be/src/main/java/yeonba/be/user/service/UserService.java
@@ -1,12 +1,10 @@
 package yeonba.be.user.service;
 
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import yeonba.be.arrow.repository.ArrowQuery;
 import yeonba.be.user.dto.response.UserProfileResponse;
-import yeonba.be.user.entity.ProfilePhoto;
 import yeonba.be.user.entity.User;
 import yeonba.be.user.repository.UserQuery;
 
@@ -23,15 +21,10 @@ public class UserService {
         User user = userQuery.findById(userId);
         User targetUser = userQuery.findById(targetUserId);
 
-        List<String> profilePhotosUrls = targetUser
-            .getProfilePhotos().stream()
-            .map(ProfilePhoto::getPhotoUrl)
-            .toList();
-
         boolean isAlreadySentArrow = arrowQuery.isArrowTransactionExist(user, targetUser);
 
         return new UserProfileResponse(
-            profilePhotosUrls,
+            targetUser.getProfilePhotoUrls(),
             targetUser.getGender(),
             targetUser.getNickname(),
             targetUser.getArrow(),
