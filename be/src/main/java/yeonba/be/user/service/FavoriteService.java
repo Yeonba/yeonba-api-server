ebce16d247996fbd796bd9705c377763bd951ba5
Minjae-An <mj3242@naver.com>
2024-03-14 19:37:37 +0900

    refactor: 코드 정렬 수정(#26)

--- be/src/main/java/yeonba/be/user/service/FavoriteService.java
+++ be/src/main/java/yeonba/be/user/service/FavoriteService.java
@@ -19,7 +19,6 @@ public class FavoriteService {
   private final FavoriteQuery favoriteQuery;
   private final UserQuery userQuery;
 
-
   /*
     즐겨찾기 등록 비즈니스 로직은 다음 예외들을 검증
     1. 자기 자신을 즐겨찾기하는 경우
