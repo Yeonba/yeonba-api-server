cbace66bac28733336bab856c6c0ac829e9d8071
Minjae-An <mj3242@naver.com>
2024-03-16 22:10:37 +0900

    feat: 전화번호 정규식 정의(#28)

--- be/src/main/java/yeonba/be/util/ServiceRegex.java
+++ be/src/main/java/yeonba/be/util/ServiceRegex.java
@@ -4,7 +4,8 @@ public enum ServiceRegex {
 
   EMAIL("[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$"),
   PASSWORD("^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[~#@!]).{8,20}$"),
-  VERIFICATION_CODE("^[A-Za-z0-9]{6}$");
+  VERIFICATION_CODE("^[A-Za-z0-9]{6}$"),
+  PHONE_NUMBER("^010\\d{8}$");
 
   private final String pattern;
 
