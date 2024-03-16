ebc659867f7c1ce459132641e52dfa759d025031
Minjae-An <mj3242@naver.com>
2024-03-16 21:51:26 +0900

    feat: 아이디 찾기 예외 enum 정의(#28)
    - 인증 코드 내역이 존재하지 않는 경우(인증 코드 만료도 포함)
    - 인증 코드가 일치하지 않는 경우

---
+++ be/src/main/java/yeonba/be/exception/LoginException.java
@@ -0,0 +1,32 @@
+package yeonba.be.exception;
+
+import org.springframework.http.HttpStatus;
+
+public enum LoginException implements BaseException {
+
+  VERIFICATION_CODE_NOT_FOUND(
+      HttpStatus.BAD_REQUEST,
+      "해당 인증 코드 내역이 존재하지 않습니다."),
+
+  VERIFICATION_CODE_NOT_MATCH(
+      HttpStatus.BAD_REQUEST,
+      "인증 코드가 일치하지 않습니다.");
+
+  private final HttpStatus httpStatus;
+  private final String reason;
+
+  LoginException(HttpStatus httpStatus, String reason) {
+    this.httpStatus = httpStatus;
+    this.reason = reason;
+  }
+
+  @Override
+  public HttpStatus getHttpStatus() {
+    return httpStatus;
+  }
+
+  @Override
+  public String getReason() {
+    return reason;
+  }
+}
