48f17d299fb1ef6375e8553b412ceb5890411274
luke <gseonggyu968@gmail.com>
2024-03-18 18:43:05 +0900

    fix: validated -> valid로 수정(#30)

--- be/src/main/java/yeonba/be/mypage/controller/MyPageController.java
+++ be/src/main/java/yeonba/be/mypage/controller/MyPageController.java
@@ -4,13 +4,13 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Size;
 import java.util.Arrays;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -103,7 +103,7 @@ public class MyPageController {
     @ApiResponse(responseCode = "204", description = "자신의 프로필 수정 요청 정상 처리")
     @PatchMapping("/users/profiles")
     public ResponseEntity<CustomResponse<Void>> updateProfile(
-        @Validated @RequestBody UserUpdateProfileRequest request,
+        @Valid @RequestBody UserUpdateProfileRequest request,
         @RequestAttribute("userId") long userId) {
 
         myPageService.updateProfile(request, userId);
@@ -190,9 +190,11 @@ public class MyPageController {
     }
 
     @Operation(summary = "휴면 계정 전환", description = "계정의 휴면 상태를 전환할 수 있습니다.")
+    @ApiResponse(responseCode = "204", description = "휴면 상태 전환 요청 정상 처리")
     @PatchMapping("/users/dormant")
     public ResponseEntity<CustomResponse<Void>> dormantUser(
-        @RequestBody UserDormantRequest request) {
+
+        @Valid @RequestBody UserDormantRequest request) {
 
         return ResponseEntity
             .ok()
