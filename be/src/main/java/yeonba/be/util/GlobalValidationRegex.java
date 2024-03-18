caeea543c3ff29e581982e954188fce2b9424004
Minjae-An <mj3242@naver.com>
2024-03-18 15:34:29 +0900

    refactor: 클래스 이름 수정(#35)
    어플리케이션 전역에서 검증을 위해 사용되는 정규식들을 모아놓는 용도를 잘 표현하기 위해
    클래스명 수정

--- be/src/main/java/yeonba/be/util/GlobalValidationRegex.java
+++ be/src/main/java/yeonba/be/util/GlobalValidationRegex.java
@@ -1,13 +1,13 @@
 package yeonba.be.util;
 
-public enum ServiceRegex {
+public enum GlobalValidationRegex {
 
   EMAIL("[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$"),
   PASSWORD("^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[~#@!]).{8,20}$");
 
   private final String pattern;
 
-  ServiceRegex(String pattern) {
+  GlobalValidationRegex(String pattern) {
     this.pattern = pattern;
   }
 
