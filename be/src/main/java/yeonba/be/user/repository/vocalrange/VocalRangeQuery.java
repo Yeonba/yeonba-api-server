c2758a1f1aeb9e36aac69691dac231ee1271ae1b
Minjae-An <mj3242@naver.com>
2024-03-21 21:20:14 +0900

    refactor: 분류 기반 음역대 조회 로직 수정(#8)
    - 잘못된 인덴트 수정, 코드 정렬
    - 회원가입 관련 예외 enum을 사용토록 로직 수정

--- be/src/main/java/yeonba/be/user/repository/vocalrange/VocalRangeQuery.java
+++ be/src/main/java/yeonba/be/user/repository/vocalrange/VocalRangeQuery.java
@@ -3,7 +3,7 @@ package yeonba.be.user.repository.vocalrange;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import yeonba.be.exception.GeneralException;
-import yeonba.be.exception.LoginException;
+import yeonba.be.exception.JoinException;
 import yeonba.be.user.entity.VocalRange;
 
 @Component
@@ -14,7 +14,7 @@ public class VocalRangeQuery {
 
   public VocalRange findBy(String classification) {
 
-    return vocalRangeRepository.findByClassification(classification)
-        .orElseThrow(() -> new GeneralException(LoginException.VOCAL_RANGE_NOT_FOUND));
+	  return vocalRangeRepository.findByClassification(classification)
+		  .orElseThrow(() -> new GeneralException(JoinException.VOCAL_RANGE_NOT_FOUND));
   }
 }
