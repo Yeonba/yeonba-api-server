d72b3dedded0e90d4b71690b93c8c838185e53d5
Minjae-An <mj3242@naver.com>
2024-03-21 21:23:04 +0900

    refactor: 이름 기반 지역 조회 로직 수정(#8)
    - 잘못된 들여쓰기, 코드 정렬 수정
    - 회원가입 관련 예외 enum 사용토록 수정

--- be/src/main/java/yeonba/be/user/repository/area/AreaQuery.java
+++ be/src/main/java/yeonba/be/user/repository/area/AreaQuery.java
@@ -3,18 +3,18 @@ package yeonba.be.user.repository.area;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import yeonba.be.exception.GeneralException;
-import yeonba.be.exception.LoginException;
+import yeonba.be.exception.JoinException;
 import yeonba.be.user.entity.Area;
 
 @Component
 @RequiredArgsConstructor
 public class AreaQuery {
 
-  private final AreaRepository areaRepository;
+	private final AreaRepository areaRepository;
 
-  public Area findByName(String name) {
+	public Area findByName(String name) {
 
-    return areaRepository.findByName(name)
-        .orElseThrow(() -> new GeneralException(LoginException.AREA_NOT_FOUND));
-  }
+		return areaRepository.findByName(name)
+			.orElseThrow(() -> new GeneralException(JoinException.AREA_NOT_FOUND));
+	}
 }
