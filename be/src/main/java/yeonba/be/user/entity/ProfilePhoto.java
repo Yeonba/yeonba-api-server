4dc2a884cd574ddfc1cf5e175e2d7352c5baead5
Minjae-An <mj3242@naver.com>
2024-03-21 21:23:55 +0900

    refactor: 잘못된 들여쓰기, 코드 정렬로 수정(#8)

--- be/src/main/java/yeonba/be/user/entity/ProfilePhoto.java
+++ be/src/main/java/yeonba/be/user/entity/ProfilePhoto.java
@@ -1,6 +1,7 @@
 package yeonba.be.user.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -8,31 +9,33 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Table(name = "profile_photos")
 @Getter
 @Entity
+@EntityListeners(value = AuditingEntityListener.class)
 @NoArgsConstructor
-@AllArgsConstructor
 public class ProfilePhoto {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @ManyToOne
-    @JoinColumn(name = "user_id")
-    private User user;
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
 
-    private String photoUrl;
-    private LocalDateTime createdAt;
+	private String photoUrl;
 
-    public ProfilePhoto(User user, String photoUrl, LocalDateTime createdAt) {
-        this.user = user;
-        this.photoUrl = photoUrl;
-        this.createdAt = createdAt;
-    }
+	@CreatedDate
+	private LocalDateTime createdAt;
+
+	public ProfilePhoto(User user, String photoUrl) {
+		this.user = user;
+		this.photoUrl = photoUrl;
+	}
 }
