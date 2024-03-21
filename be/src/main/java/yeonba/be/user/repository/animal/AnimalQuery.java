a836b55001658fb16c0ccecdd14cfea413370dd0
Minjae-An <mj3242@naver.com>
2024-03-21 21:21:09 +0900

    refactor: 이름 기반 닮은 동물상 조회 기능 수정(#8)
    - 잘못된 들여쓰기 수정, 코드 정렬
    - 회원가입 관련 예외 enum을 이용하도록 수정

--- be/src/main/java/yeonba/be/user/repository/animal/AnimalQuery.java
+++ be/src/main/java/yeonba/be/user/repository/animal/AnimalQuery.java
@@ -3,18 +3,18 @@ package yeonba.be.user.repository.animal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import yeonba.be.exception.GeneralException;
-import yeonba.be.exception.LoginException;
+import yeonba.be.exception.JoinException;
 import yeonba.be.user.entity.Animal;
 
 @Component
 @RequiredArgsConstructor
 public class AnimalQuery {
 
-  private final AnimalRepository animalRepository;
+	private final AnimalRepository animalRepository;
 
-  public Animal findByName(String name) {
+	public Animal findByName(String name) {
 
-    return animalRepository.findByName(name)
-        .orElseThrow(() -> new GeneralException(LoginException.ANIMAL_NOT_FOUND));
-  }
+		return animalRepository.findByName(name)
+			.orElseThrow(() -> new GeneralException(JoinException.ANIMAL_NOT_FOUND));
+	}
 }
