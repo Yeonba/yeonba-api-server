71c6087a9249fa2fa153c9f6cd03c5598e36f166
Minjae-An <mj3242@naver.com>
2024-03-21 21:34:40 +0900

    feat: 성별 enum 구성(#8)
    - 성별 enum 정의
    - 문자열 성별 값을 enum으로 치환할 수 있는 편의 메서드 구현

---
+++ be/src/main/java/yeonba/be/user/enums/Gender.java
@@ -0,0 +1,26 @@
+package yeonba.be.user.enums;
+
+import org.apache.commons.lang3.StringUtils;
+
+public enum Gender {
+
+  MALE("남", true),
+  FEMALE("여", false);
+
+  public final String genderString;
+  public final boolean genderBoolean;
+
+  Gender(String genderString, boolean genderBoolean) {
+    this.genderString = genderString;
+    this.genderBoolean = genderBoolean;
+  }
+
+  public static Gender fromGenderString(String genderString) {
+    if (StringUtils.equals(genderString, MALE.genderString)) {
+
+      return MALE;
+    }
+
+    return FEMALE;
+  }
+}
