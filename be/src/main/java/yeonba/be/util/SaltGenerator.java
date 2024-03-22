0a123bf808b733ca1076902f0a985c15e63bff22
Minjae-An <mj3242@naver.com>
2024-03-22 21:58:02 +0900

    feat: 랜덤 salt 생성 로직 수정(#8)
    - salt의 길이 고정 32비트(4바이트)로 변경
    - 32비트 길이 salt 만으로 대부분의 보안적 위협 충분히 커버 가능
    - base64 인코딩에 따라 로직에서 생성되는 salt 문자열의 길이는 8

--- be/src/main/java/yeonba/be/util/SaltGenerator.java
+++ be/src/main/java/yeonba/be/util/SaltGenerator.java
@@ -5,19 +5,23 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.apache.commons.codec.binary.Base64;
 
+/*
+	- 32비트(4바이트) 길이 salt 만으로 대부분의 보안적 위협은 충분히 커버 가능
+	- base64 인코딩에 따라 아래 로직에서 생성되는 salt 문자열의 길이는 8
+ */
+
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class SaltGenerator {
 
-  private static final int MIN_SALT_LENGTH = 16;
-  private static final int MAX_SALT_LENGTH = 256;
+	private static final int SALT_BYTE_LENGTH = 4;
 
-  public static String generateRandomSalt(){
-    SecureRandom random = new SecureRandom();
-    int length = random.nextInt(MIN_SALT_LENGTH, MAX_SALT_LENGTH);
+	public static String generateRandomSalt() {
 
-    byte[] salt = new byte[length];
-    random.nextBytes(salt);
+		SecureRandom random = new SecureRandom();
 
-    return Base64.encodeBase64String(salt);
-  }
+		byte[] salt = new byte[SALT_BYTE_LENGTH];
+		random.nextBytes(salt);
+
+		return Base64.encodeBase64String(salt);
+	}
 }
