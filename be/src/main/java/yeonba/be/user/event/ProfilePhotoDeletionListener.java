a7cc07da53f3eec43aa3d94bc83d083da64f8f76
Minjae-An <mj3242@naver.com>
2024-03-22 22:04:15 +0900

    feat: 프로필 사진 삭제 이벤트 리스너 정의(#8)
    - 회원가입 과정중 트랜잭션 롤백 발생시 프로필 사진 삭제 이벤트 발생
    - 이벤트를 통해 삭제 대상인 프로필 사진 엔티티들을 전달 받음
    - S3 서비스를 통해 해당 엔티티들의 삭제 작업 진행

---
+++ be/src/main/java/yeonba/be/user/event/ProfilePhotoDeletionListener.java
@@ -0,0 +1,20 @@
+package yeonba.be.user.event;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+import yeonba.be.util.S3Service;
+
+@Component
+@RequiredArgsConstructor
+public class ProfilePhotoDeletionListener {
+
+	private final S3Service s3Service;
+
+	@TransactionalEventListener(phase = TransactionPhase.AFTER_ROLLBACK)
+	public void onProfilePhotoDeletion(ProfilePhotoDeletionEvent event) {
+
+		s3Service.deleteProfilePhotos(event.getProfilePhotos());
+	}
+}
