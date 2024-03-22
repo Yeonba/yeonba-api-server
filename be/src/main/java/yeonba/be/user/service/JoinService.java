535343d4337ecefa205852c4c309cf005a4b644b
Minjae-An <mj3242@naver.com>
2024-03-22 22:06:27 +0900

    feat: 회원가입 로직 롤백시 업로드된 프로필 사진 삭제 이벤트 발행 로직 추가(#8)
    회원가입 도중 예외 상황 등으로 인해 트랜잭션 롤백시 이미 s3에 업로드된 프로필
    사진들을 삭제하기 위한 이벤트 발행 로직 추가

--- be/src/main/java/yeonba/be/user/service/JoinService.java
+++ be/src/main/java/yeonba/be/user/service/JoinService.java
@@ -1,17 +1,20 @@
 package yeonba.be.user.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import yeonba.be.login.dto.request.UserJoinRequest;
 import yeonba.be.login.dto.response.UserJoinResponse;
 import yeonba.be.user.entity.User;
+import yeonba.be.user.event.ProfilePhotoDeletionEvent;
 
 @Service
 @RequiredArgsConstructor
 public class JoinService {
 
-   	private final UserService userService;
+	private final ApplicationEventPublisher eventPublisher;
+	private final UserService userService;
 
 	/*
 	  회원 가입 비즈니스 로직 과정
@@ -23,6 +26,12 @@ public class JoinService {
 	public UserJoinResponse join(UserJoinRequest request) {
 		User user = userService.saveUser(request);
 		userService.saveProfilePhotos(user, request);
+
+		// 트랜잭션 롤백시 업로드된 프로필 사진 삭제를 위해 이벤트 발행
+		ProfilePhotoDeletionEvent event = new ProfilePhotoDeletionEvent(
+			user.getProfilePhotos());
+		eventPublisher.publishEvent(event);
+
 		userService.saveUserPreference(user, request);
 
 		return new UserJoinResponse("access_token", "refresh_token");
