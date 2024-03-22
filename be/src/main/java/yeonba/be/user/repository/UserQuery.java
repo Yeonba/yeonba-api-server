348aa8f7c66059b746587e39a17d9da44c46d7ac
Minjae-An <mj3242@naver.com>
2024-03-22 21:55:53 +0900

    feat: 이미 사용 중인 닉네임, 이메일 확인 로직 추가(#8)

--- be/src/main/java/yeonba/be/user/repository/UserQuery.java
+++ be/src/main/java/yeonba/be/user/repository/UserQuery.java
@@ -10,28 +10,38 @@ import yeonba.be.user.entity.User;
 @RequiredArgsConstructor
 public class UserQuery {
 
-  private final UserRepository userRepository;
+	private final UserRepository userRepository;
 
-  public User findById(long userId) {
+	public User findById(long userId) {
 
-    return userRepository.findById(userId)
-        .orElseThrow(() -> new GeneralException(UserException.USER_NOT_FOUND));
-  }
+		return userRepository.findById(userId)
+			.orElseThrow(() -> new GeneralException(UserException.USER_NOT_FOUND));
+	}
 
-  public User findByEmail(String email) {
+	public User findByEmail(String email) {
 
-    return userRepository.findByEmail(email)
-        .orElseThrow(() -> new GeneralException(UserException.USER_NOT_FOUND));
-  }
+		return userRepository.findByEmail(email)
+			.orElseThrow(() -> new GeneralException(UserException.USER_NOT_FOUND));
+	}
 
-  public boolean isUserExist(String phoneNumber) {
+	public boolean isUserExist(String phoneNumber) {
 
-    return userRepository.existsByPhoneNumber(phoneNumber);
-  }
+		return userRepository.existsByPhoneNumber(phoneNumber);
+	}
 
-  public User findByPhoneNumber(String phoneNumber) {
+	public User findByPhoneNumber(String phoneNumber) {
 
-    return userRepository.findByPhoneNumber(phoneNumber)
-        .orElseThrow(() -> new GeneralException(UserException.USER_NOT_FOUND));
-  }
+		return userRepository.findByPhoneNumber(phoneNumber)
+			.orElseThrow(() -> new GeneralException(UserException.USER_NOT_FOUND));
+	}
+
+	public boolean isAlreadyUsedNickname(String nickname) {
+
+		return userRepository.existsByNickname(nickname);
+	}
+
+	public boolean isAlreadyUsedEmail(String email) {
+
+		return userRepository.existsByEmail(email);
+	}
 }
