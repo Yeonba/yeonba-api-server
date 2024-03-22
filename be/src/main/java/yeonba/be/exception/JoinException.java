eb97f428bc4f8771eda7b0a36e45ba379985592a
Minjae-An <mj3242@naver.com>
2024-03-22 21:58:44 +0900

    feat: 이미 사용 중인 이메일/닉네임 예외 추가(#8)
    - 이미 사용 중인 이메일 예외 enum 정의
    - 이미 사용 중인 닉네임 예외 enum 정의

--- be/src/main/java/yeonba/be/exception/JoinException.java
+++ be/src/main/java/yeonba/be/exception/JoinException.java
@@ -6,19 +6,27 @@ public enum JoinException implements BaseException {
 
 	PASSWORD_CONFIRMATION_NOT_MATCH(
 		HttpStatus.BAD_REQUEST,
-      "비밀번호 확인 값이 비밀번호와 일치하지 않습니다."),
+		"비밀번호 확인 값이 비밀번호와 일치하지 않습니다."),
 
 	VOCAL_RANGE_NOT_FOUND(
 		HttpStatus.BAD_REQUEST,
-      "존재하지 않는 음역대입니다."),
+		"존재하지 않는 음역대입니다."),
 
 	ANIMAL_NOT_FOUND(
 		HttpStatus.BAD_REQUEST,
-      "존재하지 않는 동물상입니다."),
+		"존재하지 않는 동물상입니다."),
 
 	AREA_NOT_FOUND(
 		HttpStatus.BAD_REQUEST,
-      "존재하지 않는 지역입니다.");
+		"존재하지 않는 지역입니다."),
+
+	ALREADY_USED_EMAIL(
+		HttpStatus.BAD_REQUEST,
+		"이미 사용 중인 이메일입니다."),
+
+	ALREADY_USED_NICKNAME(
+		HttpStatus.BAD_REQUEST,
+		"이미 사용 중인 닉네임입니다.");
 
 
 	private final HttpStatus httpStatus;
