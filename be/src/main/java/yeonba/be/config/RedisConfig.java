3208dff65ffc7d74fbdb70700ce4f26f0541f116
Minjae-An <mj3242@naver.com>
2024-03-23 15:38:33 +0900

    refactor: redis 관련 설정 수정(#28)
    - redis 관련 속성, application.properties 통해 주입받도록 수정
    - 잘못된 들여쓰기 수정

--- be/src/main/java/yeonba/be/config/RedisConfig.java
+++ be/src/main/java/yeonba/be/config/RedisConfig.java
@@ -11,25 +11,25 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
-  @Value("${spring.data.redis.host}")
-  private String host;
+	@Value("${REDIS_HOST}")
+	private String host;
 
-  @Value("${spring.data.redis.port}")
-  private int port;
+	@Value("${REDIS_PORT}")
+	private int port;
 
-  @Bean
-  public RedisConnectionFactory redisConnectionFactory() {
+	@Bean
+	public RedisConnectionFactory redisConnectionFactory() {
 
-    return new LettuceConnectionFactory(host, port);
-  }
+		return new LettuceConnectionFactory(host, port);
+	}
 
-  @Bean
-  public RedisTemplate<String, Object> redisTemplate() {
-    RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-    redisTemplate.setConnectionFactory(redisConnectionFactory());
+	@Bean
+	public RedisTemplate<String, Object> redisTemplate() {
+		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+		redisTemplate.setConnectionFactory(redisConnectionFactory());
 
-    redisTemplate.setDefaultSerializer(new StringRedisSerializer());
+		redisTemplate.setDefaultSerializer(new StringRedisSerializer());
 
-    return redisTemplate;
-  }
+		return redisTemplate;
+	}
 }
