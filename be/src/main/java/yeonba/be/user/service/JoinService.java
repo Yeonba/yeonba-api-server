afdaeef889ae5a290b8b4e3aa34fc1cedfae4b70
Minjae-An <mj3242@naver.com>
2024-03-23 18:44:32 +0900

    feat: 회원가입 로직, access/refresh token 발급 로직 추가(#8)
    - 회원가입 정상 완료의 결과로 access token과 refresh token 제공
    - 회원가입 비즈니스 로직에 access token, refresh token 발급 로직 추가
    - 새 refresh token 발급시 사용자 refresh token 업데이트 수행

--- be/src/main/java/yeonba/be/user/service/JoinService.java
+++ be/src/main/java/yeonba/be/user/service/JoinService.java
@@ -1,5 +1,6 @@
 package yeonba.be.user.service;
 
+import java.util.Date;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
@@ -8,6 +9,7 @@ import yeonba.be.login.dto.request.UserJoinRequest;
 import yeonba.be.login.dto.response.UserJoinResponse;
 import yeonba.be.user.entity.User;
 import yeonba.be.user.event.ProfilePhotoDeletionEvent;
+import yeonba.be.util.JwtUtil;
 
 @Service
 @RequiredArgsConstructor
@@ -16,11 +18,15 @@ public class JoinService {
 	private final ApplicationEventPublisher eventPublisher;
 	private final UserService userService;
 
+	private final JwtUtil jwtUtil;
+
 	/*
 	  회원 가입 비즈니스 로직 과정
 	  1. 사용자 엔티티 저장
 	  2. 프로필 사진 엔티티 저장
 	  3. 사용자 선호조건 저장
+	  4. access token 및 refresh token 발급
+	  5. 사용자 refresh token 업데이트
  	*/
 	@Transactional
 	public UserJoinResponse join(UserJoinRequest request) {
@@ -34,6 +40,14 @@ public class JoinService {
 
 		userService.saveUserPreference(user, request);
 
-		return new UserJoinResponse("access_token", "refresh_token");
+		// access token, refresh token 발급
+		Date issuedAt = new Date();
+		String accessToken = jwtUtil.generateAccessToken(user, issuedAt);
+		String refreshToken = jwtUtil.generateRefreshToken(user, issuedAt);
+
+		// 사용자 refresh token 업데이트
+		user.updateRefreshToken(refreshToken);
+
+		return new UserJoinResponse(accessToken, refreshToken);
 	}
 }
