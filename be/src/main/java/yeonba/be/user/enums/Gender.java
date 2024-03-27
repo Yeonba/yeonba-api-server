c2f30c4bb27b83ef36cf47f7ce4683155a99c66a
Minjae-An <mj3242@naver.com>
2024-03-27 15:39:59 +0900

    refactor: 문자열 값 Geneder enum 변환 메서드 이름 수정(#8)

--- be/src/main/java/yeonba/be/user/enums/Gender.java
+++ be/src/main/java/yeonba/be/user/enums/Gender.java
@@ -4,23 +4,24 @@ import org.apache.commons.lang3.StringUtils;
 
 public enum Gender {
 
-  MALE("남", true),
-  FEMALE("여", false);
+	MALE("남", true),
+	FEMALE("여", false);
 
-  public final String genderString;
-  public final boolean genderBoolean;
+	public final String genderString;
+	public final boolean genderBoolean;
 
-  Gender(String genderString, boolean genderBoolean) {
-    this.genderString = genderString;
-    this.genderBoolean = genderBoolean;
-  }
+	Gender(String genderString, boolean genderBoolean) {
+		this.genderString = genderString;
+		this.genderBoolean = genderBoolean;
+	}
 
-  public static Gender fromGenderString(String genderString) {
-    if (StringUtils.equals(genderString, MALE.genderString)) {
+	public static Gender from(String genderString) {
 
-      return MALE;
-    }
+		if (StringUtils.equals(genderString, MALE.genderString)) {
 
-    return FEMALE;
-  }
+			return MALE;
+		}
+
+		return FEMALE;
+	}
 }
