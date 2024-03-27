8eb17b1286b94c57d03c926399f2711ea7c0981d
Minjae-An <mj3242@naver.com>
2024-03-27 18:31:08 +0900

    refactor: 인증 코드 요청 dto 이름 변경(#44)

--- be/src/main/java/yeonba/be/login/dto/request/UserVerificationCodeRequest.java
+++ be/src/main/java/yeonba/be/login/dto/request/UserVerificationCodeRequest.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-public class UserPhoneNumberVerifyRequest {
+public class UserVerificationCodeRequest {
 
 	@Schema(
 		type = "string",
