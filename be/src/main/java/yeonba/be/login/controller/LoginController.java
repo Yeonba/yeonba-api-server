2ad4bcc2e1193c2607b1d1b38a5adfbbc130ec89
Minjae-An <mj3242@naver.com>
2024-03-27 21:25:56 +0900

    feat: 로그인 API 구현(#50)

--- be/src/main/java/yeonba/be/login/controller/LoginController.java
+++ be/src/main/java/yeonba/be/login/controller/LoginController.java
@@ -44,38 +44,30 @@ public class LoginController {
 			.body(new CustomResponse<>(response));
 	}
 
-	@Operation(
-		summary = "전화번호 인증 코드 전송",
-		description = "전화번호 인증을 위해 해당 번호로 인증 코드를 발송합니다."
-	)
-	@ApiResponse(
-		responseCode = "204",
-		description = "전화번호 인증 코드 전송 성공"
-	)
+	@Operation(summary = "전화번호 인증 코드 전송", description = "전화번호 인증을 위해 해당 번호로 인증 코드를 발송합니다.")
+	@ApiResponse(responseCode = "202", description = "전화번호 인증 코드 전송 성공")
 	@PostMapping("/users/help/id-inquiry/verification-code")
 	public ResponseEntity<CustomResponse<Void>> verifyPhoneNumber(
 		@RequestBody UserPhoneNumberVerifyRequest request) {
+
+		loginService.sendVerificationCodeMessage(request);
 
 		return ResponseEntity
 			.accepted()
 			.body(new CustomResponse<>());
 	}
 
-	@Operation(
-		summary = "아이디 찾기",
-		description = "인증 코드를 바탕으로 아이디를 찾을 수 있습니다."
-	)
-	@ApiResponse(
-		responseCode = "200",
-		description = "아이디 찾기 정상 처리"
-	)
+	@Operation(summary = "아이디 찾기", description = "인증 코드를 바탕으로 아이디를 찾을 수 있습니다.")
+	@ApiResponse(responseCode = "200", description = "아이디 찾기 정상 처리")
 	@PostMapping("/users/help/id-inquiry")
 	public ResponseEntity<CustomResponse<UserIdInquiryResponse>> idInquiry(
 		@RequestBody UserIdInquiryRequest request) {
 
+		UserIdInquiryResponse response = loginService.findEmail(request);
+
 		return ResponseEntity
 			.ok()
-			.body(new CustomResponse<>(new UserIdInquiryResponse("mj3242@naver.com")));
+			.body(new CustomResponse<>(response));
 	}
 
 	@Operation(summary = "비밀번호 찾기", description = "이메일로 임시 비밀번호를 발급받을 수 있습니다.")
@@ -92,23 +84,16 @@ public class LoginController {
 	}
 
 	@Operation(summary = "로그인", description = "로그인을 할 수 있습니다.")
+	@ApiResponse(responseCode = "200", description = "로그인 성공")
 	@PostMapping("/users/login")
 	public ResponseEntity<CustomResponse<UserLoginResponse>> login(
-		@RequestBody UserLoginRequest request) {
+		@Valid @RequestBody UserLoginRequest request) {
+
+		UserLoginResponse response = loginService.login(request);
 
 		return ResponseEntity
 			.ok()
-			.body(new CustomResponse<>(
-				new UserLoginResponse(
-					"""
-						eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9
-						.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ
-						.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c""",
-					"""
-						eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9
-						.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ
-						.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"""
-				)));
+			.body(new CustomResponse<>(response));
 	}
 
 	@Operation(
