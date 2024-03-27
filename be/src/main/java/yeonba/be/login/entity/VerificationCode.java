c936d656582f0541f1e0e314d78993bde9e4f4e2
Minjae-An <mj3242@naver.com>
2024-03-27 15:02:00 +0900

    feat: 인증 코드 엔티티 구성(#28)
    - 발급 받은 전화번호, 인증 코드, 만료 일자를 포함하는 인증 코드 엔티티 정의
    - 만료 여부 확인 로직 추가

---
+++ be/src/main/java/yeonba/be/login/entity/VerificationCode.java
@@ -0,0 +1,45 @@
+package yeonba.be.login.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class VerificationCode {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String phoneNumber;
+
+	@Column(nullable = false)
+	private String code;
+
+	@Column(nullable = false)
+	private LocalDateTime expiredAt;
+
+	public VerificationCode(
+		String phoneNumber,
+		String code,
+		LocalDateTime expiredAt) {
+
+		this.phoneNumber = phoneNumber;
+		this.code = code;
+		this.expiredAt = expiredAt;
+	}
+
+	public boolean isExpired(LocalDateTime now) {
+
+		return this.expiredAt.isBefore(now);
+	}
+}
