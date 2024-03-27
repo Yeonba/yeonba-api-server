eac6a0ecaee16756c956837ff408198f62e0438d
Minjae-An <mj3242@naver.com>
2024-03-27 19:52:32 +0900

    feat: 공백값, null 값 검증 조건 추가(#28)

--- be/src/main/java/yeonba/be/login/dto/request/UserEmailInquiryRequest.java
+++ be/src/main/java/yeonba/be/login/dto/request/UserEmailInquiryRequest.java
@@ -1,6 +1,7 @@
 package yeonba.be.login.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,6 +17,7 @@ public class UserEmailInquiryRequest {
 	@Pattern(
 		regexp = "^010\\d{8}$",
 		message = "전화번호는 11자리 010으로 시작하며 하이픈(-) 없이 0~9의 숫자로 이뤄져야 합니다.")
+	@NotBlank(message = "전화번호는 반드시 입력되어야 합니다.")
 	private String phoneNumber;
 
 	@Schema(
@@ -25,5 +27,6 @@ public class UserEmailInquiryRequest {
 	@Pattern(
 		regexp = "^[A-Za-z0-9]{6}$",
 		message = "인증 코드는 6자리로 영어대소문자, 숫자로만 이뤄져야 합니다.")
+	@NotBlank(message = "인증 코드는 반드시 입력되어야 합니다.")
 	private String verificationCode;
 }
