2d38e0da57f1ebe9d71c6729769c77c12265b9b4
Minjae-An <mj3242@naver.com>
2024-03-27 19:58:57 +0900

    feat: 테이블 매핑 설정 추가(#28)

--- be/src/main/java/yeonba/be/login/entity/VerificationCode.java
+++ be/src/main/java/yeonba/be/login/entity/VerificationCode.java
@@ -5,11 +5,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Table(name = "verification_codes")
 @Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
