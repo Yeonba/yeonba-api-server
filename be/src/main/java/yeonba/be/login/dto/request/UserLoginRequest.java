3c40dbb5623e26d346d7ce4e4020deac68688538
Minjae-An <mj3242@naver.com>
2024-03-27 21:21:41 +0900

    feat: 로그인 요청 dto, 검증 조건 추가(#50)
    dto 필드들에 다음 검증 조건들을 추가하였다.
    - 이메일 공백값, null 값 검증 조건
    - 이메일 정규식 검증 조건
    - 비밀번호 공배값, null 값 검증 조건
    - 비밀번호 정규식 검증 조건

--- be/src/main/java/yeonba/be/login/dto/request/UserLoginRequest.java
+++ be/src/main/java/yeonba/be/login/dto/request/UserLoginRequest.java
@@ -1,26 +1,34 @@
 package yeonba.be.login.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
 public class UserLoginRequest {
 
-  @Schema(
-      type = "string",
-      description = "이메일",
-      example = "mj3242@naver.com"
-  )
-  private String email;
+	@Schema(
+		type = "string",
+		description = "이메일",
+		example = "mj3242@naver.com")
+	@Pattern(
+		regexp = "[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$",
+		message = "유효하지 않은 이메일 형식입니다.")
+	@NotBlank(message = "이메일은 반드시 입력되어야 합니다.")
+	private String email;
 
-  @Schema(
-      type = "string",
-      description = "비밀번호",
-      example = "examplePassword!"
-  )
-  private String password;
+	@Schema(
+		type = "string",
+		description = "비밀번호",
+		example = "examplePassword!")
+	@Pattern(
+		regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[~#@!]).{8,20}$",
+		message = """
+			비밀번호는 영어대소문자, 숫자, 특수문자(~#@!)를
+			최소 1자씩 포함하며 8~20자 사이여야 합니다.""")
+	@NotBlank(message = "비밀번호는 반드시 입력되어야 합니다.")
+	private String password;
 }
