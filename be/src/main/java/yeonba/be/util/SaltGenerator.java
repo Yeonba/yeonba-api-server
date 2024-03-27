f90f944c937f9f2274bd8d9ae13c7b54032f5c9b
Minjae-An <mj3242@naver.com>
2024-03-27 16:24:07 +0900

    feat: salt 길이 32바이트로 변경(#8)
    보안적으로 안전한 32자로 salt 길이 변경

--- be/src/main/java/yeonba/be/util/SaltGenerator.java
+++ be/src/main/java/yeonba/be/util/SaltGenerator.java
@@ -6,14 +6,14 @@ import lombok.NoArgsConstructor;
 import org.apache.commons.codec.binary.Base64;
 
 /*
-	- 32비트(4바이트) 길이 salt 만으로 대부분의 보안적 위협은 충분히 커버 가능
-	- base64 인코딩에 따라 아래 로직에서 생성되는 salt 문자열의 길이는 8
+	- 32바이트 길이의 salt만으로 대부분의 보안적 위협은 커버 가능
+	- 이하 로직을 통해 생성되는 salt 문자열의 길이는 약 44자
  */
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class SaltGenerator {
 
-	private static final int SALT_BYTE_LENGTH = 4;
+	private static final int SALT_BYTE_LENGTH = 32;
 
 	public static String generateRandomSalt() {
 
