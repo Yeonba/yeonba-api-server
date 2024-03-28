581240f0307d8714a3fb5e19af2e6e02437b0b6e
Minjae-An <mj3242@naver.com>
2024-03-28 17:20:33 +0900

    refactor: access token 재발급 요청 dto 수정(#50)
    - refresh token 필드 예시 값 수정
    - 공백/null 값 검증 조건 추가
    - 불필요한 생성자 삭제

--- be/src/main/java/yeonba/be/login/dto/request/UserRefreshTokenRequest.java
+++ be/src/main/java/yeonba/be/login/dto/request/UserRefreshTokenRequest.java
@@ -1,21 +1,18 @@
 package yeonba.be.login.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
 public class UserRefreshTokenRequest {
-	@Schema(
-		type = "string",
-		example = """
-			eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9
-			.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ
-			.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c""",
-		description = "access token 재발급을 위한 refresh token"
-	)
-	private String refreshToken;
+
+    @Schema(
+        type = "string",
+        example = "header.payload.signature",
+        description = "access token 재발급을 위한 refresh token")
+    @NotBlank(message = "refresh token은 반드시 입력되어야 합니다.")
+    private String refreshToken;
 }
