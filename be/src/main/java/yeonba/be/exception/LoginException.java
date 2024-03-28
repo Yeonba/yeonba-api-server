b6159b94fa3eb771206c97b78cbcec50dfd41d5b
Minjae-An <mj3242@naver.com>
2024-03-28 17:29:14 +0900

    feat: refresh token 불일치 예외 정의(#50)

--- be/src/main/java/yeonba/be/exception/LoginException.java
+++ be/src/main/java/yeonba/be/exception/LoginException.java
@@ -4,33 +4,38 @@ import org.springframework.http.HttpStatus;
 
 public enum LoginException implements BaseException {
 
-	VERIFICATION_CODE_NOT_FOUND(
-		HttpStatus.BAD_REQUEST,
-		"해당 인증 코드 내역이 존재하지 않습니다."),
+    VERIFICATION_CODE_NOT_FOUND(
+        HttpStatus.BAD_REQUEST,
+        "해당 인증 코드 내역이 존재하지 않습니다."),
 
-	VERIFICATION_CODE_NOT_MATCH(
-		HttpStatus.BAD_REQUEST,
-		"인증 코드가 일치하지 않습니다."),
+    VERIFICATION_CODE_NOT_MATCH(
+        HttpStatus.BAD_REQUEST,
+        "인증 코드가 일치하지 않습니다."),
 
-	PASSWORD_NOT_MATCH(
-		HttpStatus.BAD_REQUEST,
-		"비밀번호가 일치하지 않습니다.");
+    PASSWORD_NOT_MATCH(
+        HttpStatus.BAD_REQUEST,
+        "비밀번호가 일치하지 않습니다."),
 
-	private final HttpStatus httpStatus;
-	private final String reason;
+    REFRESH_TOKEN_NOT_MATCH(
+        HttpStatus.BAD_REQUEST,
+        "refresh token이 일치하지 않습니다.");
 
-	LoginException(HttpStatus httpStatus, String reason) {
-		this.httpStatus = httpStatus;
-		this.reason = reason;
-	}
+    private final HttpStatus httpStatus;
+    private final String reason;
 
-	@Override
-	public HttpStatus getHttpStatus() {
-		return httpStatus;
-	}
+    LoginException(HttpStatus httpStatus, String reason) {
 
-	@Override
-	public String getReason() {
-		return reason;
-	}
+        this.httpStatus = httpStatus;
+        this.reason = reason;
+    }
+
+    @Override
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+    @Override
+    public String getReason() {
+        return reason;
+    }
 }
