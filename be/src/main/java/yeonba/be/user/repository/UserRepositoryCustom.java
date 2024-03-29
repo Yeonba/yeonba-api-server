2943cefa8e33f21bdf1ad8361e9d03a32377f538
Minjae-An <mj3242@naver.com>
2024-03-29 17:24:13 +0900

    feat: 즐겨찾기한 사용자 목록 조회 로직 정의(#43)

---
+++ be/src/main/java/yeonba/be/user/repository/UserRepositoryCustom.java
@@ -0,0 +1,10 @@
+package yeonba.be.user.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import yeonba.be.user.dto.response.UserQueryResponse;
+
+public interface UserRepositoryCustom {
+
+    Page<UserQueryResponse> findAllFavorites(long userId, PageRequest pageRequest);
+}
