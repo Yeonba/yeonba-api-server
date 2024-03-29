cda1005ae9c56b0c21fb9e5dfd88839798c88931
Minjae-An <mj3242@naver.com>
2024-03-29 18:07:39 +0900

    feat: 이성 목록 조회 API, 즐겨찾기한 사용자 조회 로직 추가(#43)

--- be/src/main/java/yeonba/be/user/controller/UserController.java
+++ be/src/main/java/yeonba/be/user/controller/UserController.java
@@ -4,7 +4,9 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -29,106 +31,106 @@ import yeonba.be.util.CustomResponse;
 @RequiredArgsConstructor
 public class UserController {
 
-	private final BlockService blockService;
-	private final FavoriteService favoriteService;
-	private final ReportService reportService;
-	private final UserService userService;
+    private final BlockService blockService;
+    private final FavoriteService favoriteService;
+    private final ReportService reportService;
+    private final UserService userService;
 
-	@Operation(
-		summary = "이성(다른 사용자) 목록 조회",
-		description = "조건에 따라 다른 사용자 프로필 목록을 조회할 수 있습니다."
-	)
-	@ApiResponse(
-		responseCode = "200",
-		description = "이성 목록 정상 조회"
-	)
-	@GetMapping("/users")
-	public ResponseEntity<CustomResponse<UserQueryPageResponse>> users(
-		@ParameterObject UserQueryRequest request) {
+    @Operation(summary = "이성(다른 사용자) 목록 조회", description = "조건에 따라 다른 사용자 프로필 목록을 조회 가능")
+    @ApiResponse(responseCode = "200", description = "이성 목록 정상 조회")
+    @GetMapping("/users")
+    public ResponseEntity<CustomResponse<UserQueryPageResponse>> users(
+        @RequestAttribute("userId") long userId,
+        @Valid @ParameterObject UserQueryRequest request) {
 
-		return ResponseEntity
-			.ok()
-			.body(new CustomResponse<>());
-	}
+        String type = request.getType();
+        UserQueryPageResponse response = null;
 
+        if (StringUtils.equals(type, "BOOKMARKED")) {
+            response = userService.findAllFavorites(userId, request);
+        }
 
-	@Operation(summary = "다른 사용자 프로필 조회", description = "다른 사용자의 프로필을 조회할 수 있습니다.")
-	@ApiResponse(responseCode = "200", description = "사용자 프로필 정상 조회")
-	@GetMapping("/users/{userId}")
-	public ResponseEntity<CustomResponse<UserProfileResponse>> profile(
-		@RequestAttribute("userId") long userId,
-		@Parameter(description = "조회대상 사용자 ID", example = "1")
-		@PathVariable("userId") long targetUserId) {
+        return ResponseEntity
+            .ok()
+            .body(new CustomResponse<>(response));
+    }
 
-		UserProfileResponse response = userService.getTargetUserProfile(userId, targetUserId);
+    @Operation(summary = "다른 사용자 프로필 조회", description = "다른 사용자의 프로필을 조회할 수 있습니다.")
+    @ApiResponse(responseCode = "200", description = "사용자 프로필 정상 조회")
+    @GetMapping("/users/{userId}")
+    public ResponseEntity<CustomResponse<UserProfileResponse>> profile(
+        @RequestAttribute("userId") long userId,
+        @Parameter(description = "조회대상 사용자 ID", example = "1")
+        @PathVariable("userId") long targetUserId) {
 
-		return ResponseEntity
-			.ok()
-			.body(new CustomResponse<>(response));
-	}
+        UserProfileResponse response = userService.getTargetUserProfile(userId, targetUserId);
 
-	@Operation(summary = "즐겨찾기 등록", description = "다른 사용자를 자신의 즐겨찾기에 등록할 수 있습니다.")
-	@ApiResponse(responseCode = "202", description = "즐겨찾기 등록 정상 처리")
-	@PostMapping("/favorites/{userId}")
-	public ResponseEntity<CustomResponse<Void>> registerFavorite(
-		@RequestAttribute("userId") long userId,
-		@Parameter(description = "즐겨찾기에 등록될 사용자 ID", example = "1")
-		@PathVariable("userId") long favoriteUserId) {
+        return ResponseEntity
+            .ok()
+            .body(new CustomResponse<>(response));
+    }
 
-		favoriteService.addFavorite(userId, favoriteUserId);
+    @Operation(summary = "즐겨찾기 등록", description = "다른 사용자를 자신의 즐겨찾기에 등록할 수 있습니다.")
+    @ApiResponse(responseCode = "202", description = "즐겨찾기 등록 정상 처리")
+    @PostMapping("/favorites/{userId}")
+    public ResponseEntity<CustomResponse<Void>> registerFavorite(
+        @RequestAttribute("userId") long userId,
+        @Parameter(description = "즐겨찾기에 등록될 사용자 ID", example = "1")
+        @PathVariable("userId") long favoriteUserId) {
 
-		return ResponseEntity
-			.accepted()
-			.body(new CustomResponse<>());
-	}
+        favoriteService.addFavorite(userId, favoriteUserId);
 
-	@Operation(summary = "즐겨찾기 삭제", description = "즐겨찾기에 등록한 사용자를 삭제합니다.")
-	@ApiResponse(responseCode = "202", description = "즐겨찾기 삭제 정상 처리")
-	@DeleteMapping("/favorites/{userId}")
-	public ResponseEntity<CustomResponse<Void>> deleteFavorite(
-		@RequestAttribute("userId") long userId,
-		@Parameter(description = "즐겨찾기에서 삭제할 사용자 ID", example = "1")
-		@PathVariable("userId") long favoriteUserId) {
+        return ResponseEntity
+            .accepted()
+            .body(new CustomResponse<>());
+    }
 
-		favoriteService.deleteFavorite(userId, favoriteUserId);
+    @Operation(summary = "즐겨찾기 삭제", description = "즐겨찾기에 등록한 사용자를 삭제합니다.")
+    @ApiResponse(responseCode = "202", description = "즐겨찾기 삭제 정상 처리")
+    @DeleteMapping("/favorites/{userId}")
+    public ResponseEntity<CustomResponse<Void>> deleteFavorite(
+        @RequestAttribute("userId") long userId,
+        @Parameter(description = "즐겨찾기에서 삭제할 사용자 ID", example = "1")
+        @PathVariable("userId") long favoriteUserId) {
 
-		return ResponseEntity
-			.accepted()
-			.body(new CustomResponse<>());
-	}
+        favoriteService.deleteFavorite(userId, favoriteUserId);
 
+        return ResponseEntity
+            .accepted()
+            .body(new CustomResponse<>());
+    }
 
-	@Operation(summary = "사용자 신고", description = "다른 사용자를 신고할 수 있습니다.")
-	@ApiResponse(responseCode = "202", description = "신고 정상 처리")
-	@PostMapping("/users/{userId}/report")
-	public ResponseEntity<CustomResponse<Void>> report(
-		@RequestAttribute("userId") long userId,
-		@Parameter(description = "신고 대상 사용자 ID", example = "1")
-		@PathVariable("userId") long reportedUserId,
-		@RequestBody UserReportRequest request) {
+    @Operation(summary = "사용자 신고", description = "다른 사용자를 신고할 수 있습니다.")
+    @ApiResponse(responseCode = "202", description = "신고 정상 처리")
+    @PostMapping("/users/{userId}/report")
+    public ResponseEntity<CustomResponse<Void>> report(
+        @RequestAttribute("userId") long userId,
+        @Parameter(description = "신고 대상 사용자 ID", example = "1")
+        @PathVariable("userId") long reportedUserId,
+        @RequestBody UserReportRequest request) {
 
-		reportService.makeReport(
-			userId,
-			reportedUserId,
-			request);
+        reportService.makeReport(
+            userId,
+            reportedUserId,
+            request);
 
-		return ResponseEntity
-			.accepted()
-			.body(new CustomResponse<>());
-	}
+        return ResponseEntity
+            .accepted()
+            .body(new CustomResponse<>());
+    }
 
-	@Operation(summary = "차단하기", description = "다른 사용자를 차단할 수 있습니다.")
-	@ApiResponse(responseCode = "202", description = "차단 요청 정상 처리")
-	@PostMapping("/users/{userId}/block")
-	public ResponseEntity<CustomResponse<Void>> block(
-		@RequestAttribute("userId") long userId,
-		@Parameter(description = "차단하는 사용자 ID", example = "1")
-		@PathVariable("userId") long blockedUserId) {
+    @Operation(summary = "차단하기", description = "다른 사용자를 차단할 수 있습니다.")
+    @ApiResponse(responseCode = "202", description = "차단 요청 정상 처리")
+    @PostMapping("/users/{userId}/block")
+    public ResponseEntity<CustomResponse<Void>> block(
+        @RequestAttribute("userId") long userId,
+        @Parameter(description = "차단하는 사용자 ID", example = "1")
+        @PathVariable("userId") long blockedUserId) {
 
-		blockService.block(userId, blockedUserId);
+        blockService.block(userId, blockedUserId);
 
-		return ResponseEntity
-			.accepted()
-			.body(new CustomResponse<>());
-	}
+        return ResponseEntity
+            .accepted()
+            .body(new CustomResponse<>());
+    }
 }
