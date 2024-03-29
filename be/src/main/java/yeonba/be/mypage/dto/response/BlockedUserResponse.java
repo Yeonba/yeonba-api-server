38b6dbdf7e3bfa8ef523d182b5e3b25b4c6562fc
luke <gseonggyu968@gmail.com>
2024-03-29 21:15:08 +0900

    chore: API 명세를 위해 response에 설명 추가(#30)

--- be/src/main/java/yeonba/be/mypage/dto/response/BlockedUserResponse.java
+++ be/src/main/java/yeonba/be/mypage/dto/response/BlockedUserResponse.java
@@ -1,5 +1,6 @@
 package yeonba.be.mypage.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import yeonba.be.user.entity.User;
@@ -8,8 +9,22 @@ import yeonba.be.user.entity.User;
 @AllArgsConstructor
 public class BlockedUserResponse {
 
+    @Schema(
+        type = "number",
+        description = "사용자 ID",
+        example = "1")
     private long id;
+
+    @Schema(
+        type = "string",
+        description = "프로필 사진 URL",
+        example = "https://yeonba.com/profile/1")
     private String profileUrl;
+
+    @Schema(
+        type = "string",
+        description = "사용자 이름",
+        example = "안민재")
     private String name;
 
     public BlockedUserResponse(User blockedUser) {
--- be/src/main/java/yeonba/be/mypage/dto/response/BlockedUsersResponse.java
+++ be/src/main/java/yeonba/be/mypage/dto/response/BlockedUsersResponse.java
@@ -1,5 +1,6 @@
 package yeonba.be.mypage.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -8,5 +9,6 @@ import lombok.Getter;
 @AllArgsConstructor
 public class BlockedUsersResponse {
 
+    @Schema(type = "array", description = "차단한 사용자 목록")
     private List<BlockedUserResponse> blockedUsers;
 }
