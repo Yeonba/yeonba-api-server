fdd49cad1a9f4c12914329b023645a8dd6bdb692
kim seonggyu <71162390+acceptor-gyu@users.noreply.github.com>
2024-03-30 16:36:49 +0900

    [화살 충전] 광고 시청을 통한 화살 충전 (#47)
    * chore: 코드 정렬(#46)
    
    * feat: querydsl 의존성 추가(#46)
    
    * feat: 광고 시청으로 화살 충전 API 생성(#46)
    
    사용자 삭제 로직 추가 후 사용자 조회 쿼리 수정
    
    * fix: deprecated 메서드 수정(#46)

---
+++ be/src/main/java/yeonba/be/arrow/repository/ArrowTransactionRepositoryImpl.java
@@ -0,0 +1,35 @@
+package yeonba.be.arrow.repository;
+
+import static yeonba.be.arrow.entity.QArrowTransaction.arrowTransaction;
+import static yeonba.be.user.entity.QUser.user;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.time.LocalDateTime;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class ArrowTransactionRepositoryImpl implements ArrowTransactionRepositoryCustom {
+
+    private final int MAX_ARROW_COUNT_OF_AD = 3;
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public boolean canChargeByAdvertisement(long userId, LocalDateTime today) {
+
+        long countOfTodayAdView = Optional.ofNullable(
+                queryFactory
+                    .select(arrowTransaction.count())
+                    .from(arrowTransaction)
+                    .innerJoin(arrowTransaction.receivedUser, user)
+                    .where(
+                        arrowTransaction.sentUser.isNull(),
+                        arrowTransaction.receivedUser.id.eq(userId),
+                        arrowTransaction.createdAt.after(today)
+                    )
+                    .fetchOne())
+            .orElse(0L);
+
+        return countOfTodayAdView < MAX_ARROW_COUNT_OF_AD;
+    }
+}
