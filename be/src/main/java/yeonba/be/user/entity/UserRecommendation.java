febabc9f5b5a47c4a0e8d5dd79cdf92b2f60229a
Minjae-An <mj3242@naver.com>
2024-03-31 20:32:30 +0900

    feat: 사용자 추천 내역 엔티티 수정(#43)
    - 엔티티 리스너 활용, 엔티티 저장시 createdAt 필드 자동 설정
    - 기존 생성자 제거, 적절한 새 생성자 구성

--- be/src/main/java/yeonba/be/user/entity/UserRecommendation.java
+++ be/src/main/java/yeonba/be/user/entity/UserRecommendation.java
@@ -1,6 +1,7 @@
 package yeonba.be.user.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -8,15 +9,17 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
-import lombok.AllArgsConstructor;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Table(name = "users_recommendations")
 @Getter
 @Entity
-@NoArgsConstructor
-@AllArgsConstructor
+@EntityListeners(value = AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserRecommendation {
 
     @Id
@@ -31,5 +34,12 @@ public class UserRecommendation {
     @JoinColumn(name = "recommended_user_id")
     private User recommendedUser;
 
+    @CreatedDate
     private LocalDateTime createdAt;
+
+    public UserRecommendation(User user, User recommendedUser) {
+
+        this.user = user;
+        this.recommendedUser = recommendedUser;
+    }
 }
