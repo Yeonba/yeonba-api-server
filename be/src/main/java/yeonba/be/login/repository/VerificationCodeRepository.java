2084350e1b389ae270fa14428d2c0bf18e39ec82
Minjae-An <mj3242@naver.com>
2024-04-01 15:28:40 +0900

    feat: 인증 코드 조회 로직 변경(#44)
    - 기존 로직은 만료 일자를 고려하지 않고 인증 코드 조회
    - 만료 일자를 고려하여 가장 먼저 조회되는 인증 코드를 반환하는 방식으로 로직 변경

--- be/src/main/java/yeonba/be/login/repository/VerificationCodeRepository.java
+++ be/src/main/java/yeonba/be/login/repository/VerificationCodeRepository.java
@@ -1,10 +1,14 @@
 package yeonba.be.login.repository;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import yeonba.be.login.entity.VerificationCode;
 
 public interface VerificationCodeRepository extends JpaRepository<VerificationCode, Long> {
 
-    Optional<VerificationCode> findByPhoneNumberAndCode(String phoneNumber, String code);
+    Optional<VerificationCode> findFirstByPhoneNumberAndCodeAndExpiredAtIsAfter(
+        String phoneNumber,
+        String code,
+        LocalDateTime verifyAt);
 }
