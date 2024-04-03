1f642f6d3e409c8a7217d85742732d1bd3cd99fe
Minjae-An <mj3242@naver.com>
2024-04-03 19:51:07 +0900

    feat: 추천할 사용자가 없는 경우 예외 정의(#43)
    추천할 사용자가 1명이하인 예외 상황 정의

--- be/src/main/java/yeonba/be/exception/UserException.java
+++ be/src/main/java/yeonba/be/exception/UserException.java
@@ -4,25 +4,32 @@ import org.springframework.http.HttpStatus;
 
 public enum UserException implements BaseException {
 
-  USER_NOT_FOUND(
-      HttpStatus.BAD_REQUEST,
-      "해당 사용자가 존재하지 않습니다.");
+    USER_NOT_FOUND(
+        HttpStatus.BAD_REQUEST,
+        "해당 사용자가 존재하지 않습니다."),
 
-  private final HttpStatus httpStatus;
-  private final String reason;
+    NO_MORE_USERS_TO_RECOMMEND(
+        HttpStatus.BAD_REQUEST,
+        "더 이상 추천할 사용자가 존재하지 않습니다");
 
-  UserException(HttpStatus httpStatus, String reason) {
-    this.httpStatus = httpStatus;
-    this.reason = reason;
-  }
+    private final HttpStatus httpStatus;
+    private final String reason;
 
-  @Override
-  public HttpStatus getHttpStatus() {
-    return httpStatus;
-  }
+    UserException(HttpStatus httpStatus, String reason) {
 
-  @Override
-  public String getReason() {
-    return reason;
-  }
+        this.httpStatus = httpStatus;
+        this.reason = reason;
+    }
+
+    @Override
+    public HttpStatus getHttpStatus() {
+
+        return httpStatus;
+    }
+
+    @Override
+    public String getReason() {
+
+        return reason;
+    }
 }
