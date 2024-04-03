a2c43a4690be268db4e9190b0aae766cf0f34c95
Minjae-An <mj3242@naver.com>
2024-04-03 15:10:42 +0900

    refactor: 이미 사용하는 전화번호 검증 메서드명 수정(#8)
    이미 사용하는 전화번호인지 확인하는 작업을 수행한다는 것을 잘 표현하기 위해 수정

--- be/src/main/java/yeonba/be/user/repository/UserQuery.java
+++ be/src/main/java/yeonba/be/user/repository/UserQuery.java
@@ -26,7 +26,7 @@ public class UserQuery {
             .orElseThrow(() -> new GeneralException(UserException.USER_NOT_FOUND));
     }
 
-    public boolean existByPhoneNumber(String phoneNumber) {
+    public boolean isAlreadyUsedPhoneNumber(String phoneNumber) {
 
         return userRepository.existsByPhoneNumber(phoneNumber);
     }
