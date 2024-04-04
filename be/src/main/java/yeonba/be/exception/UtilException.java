6b485c496c8d9ed85cbbd3fca5f64aa4bc44c315
Minjae-An <mj3242@naver.com>
2024-04-04 18:40:32 +0900

    refactor: EOL 추가, 코드 정렬(#8)

--- be/src/main/java/yeonba/be/exception/UtilException.java
+++ be/src/main/java/yeonba/be/exception/UtilException.java
@@ -3,6 +3,7 @@ package yeonba.be.exception;
 import org.springframework.http.HttpStatus;
 
 public enum UtilException implements BaseException {
+
     INVALID_JWT(
         HttpStatus.BAD_REQUEST,
         "유효하지 않은 JWT입니다. 다시 로그인 해주세요."),
