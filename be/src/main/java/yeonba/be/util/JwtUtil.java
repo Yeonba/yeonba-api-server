b8511ca424ef8661461c145d4dee58cf6c69c787
Minjae-An <mj3242@naver.com>
2024-04-04 18:40:00 +0900

    refactor: jwt 검증 로직 삭제 및 로직 리팩토링(#8)
    - 일단 사용하지 않는 jwt 검증 로직 삭제
    - issuedAt 변수명, generatedAt으로 일괄 수정

--- be/src/main/java/yeonba/be/util/JwtUtil.java
+++ be/src/main/java/yeonba/be/util/JwtUtil.java
@@ -8,8 +8,6 @@ import java.time.temporal.ChronoUnit;
 import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import yeonba.be.exception.GeneralException;
-import yeonba.be.exception.UtilException;
 import yeonba.be.user.entity.User;
 
 @Component
@@ -21,18 +19,18 @@ public class JwtUtil {
     @Value("${JWT_SECRET}")
     private String jwtSecret;
 
-    public String generateAccessToken(User user, Date issuedAt) {
+    public String generateAccessToken(User user, Date generatedAt) {
 
-        Date expiredAt = getExpiredAt(issuedAt, ACCESS_TOKEN_DURATION);
+        Date expiredAt = getExpiredAt(generatedAt, ACCESS_TOKEN_DURATION);
 
-        return generateUserJwt(user, issuedAt, expiredAt);
+        return generateUserJwt(user, generatedAt, expiredAt);
     }
 
-    public String generateRefreshToken(User user, Date issuedAt) {
+    public String generateRefreshToken(User user, Date generatedAt) {
 
-        Date expiredAt = getExpiredAt(issuedAt, REFRESH_TOKEN_DURATION);
+        Date expiredAt = getExpiredAt(generatedAt, REFRESH_TOKEN_DURATION);
 
-        return generateUserJwt(user, issuedAt, expiredAt);
+        return generateUserJwt(user, generatedAt, expiredAt);
     }
 
     private Date getExpiredAt(Date generatedAt, Duration duration) {
@@ -51,18 +49,5 @@ public class JwtUtil {
             .claim("userId", user.getId())
             .signWith(SignatureAlgorithm.HS256, jwtSecret)
             .compact();
-    }
-
-    public void validateJwt(String jwt) {
-
-        try {
-            Jwts.parser()
-                .setSigningKey(jwtSecret)
-                .parseClaimsJws(jwt);
-
-        } catch (Exception e) {
-
-            throw new GeneralException(UtilException.INVALID_JWT);
-        }
     }
 }
