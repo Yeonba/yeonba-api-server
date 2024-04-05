88a31307f939de9fc20154ac3644eeadc21af37f
Minjae-An <mj3242@naver.com>
2024-04-05 17:22:22 +0900

    refactor: 주석 및 코드 정렬 수정(#50)

--- be/src/main/java/yeonba/be/config/BearerTokenAuthInterceptor.java
+++ be/src/main/java/yeonba/be/config/BearerTokenAuthInterceptor.java
@@ -23,8 +23,7 @@ public class BearerTokenAuthInterceptor implements HandlerInterceptor {
     access token을 이용한 인가는 이하 과정을 거쳐 이뤄진다.
     1. Authorization 헤더 값이 존재하는 지 확인
     2. Bearer 토큰 형식이 맞는 지 확인
-    3. jwt 검증(형식, 시그니처, 만료 여부)
-    4. access token 파싱, userId 파싱
+    4. access token 파싱, jwt 검증 및 userId 파싱
     5. userId request attribute 설정
      */
     @Override
@@ -36,6 +35,7 @@ public class BearerTokenAuthInterceptor implements HandlerInterceptor {
         // authorization 헤더 값 존재여부 확인
         String bearerToken = request.getHeader("Authorization");
         if (!StringUtils.hasText(bearerToken)) {
+
             throw new GeneralException(LoginException.ACCESS_TOKEN_NOT_EXIST);
         }
 
@@ -43,6 +43,7 @@ public class BearerTokenAuthInterceptor implements HandlerInterceptor {
         String regex = ServiceRegex.BEARER_TOKEN.getPattern();
         Matcher matcher = Pattern.compile(regex).matcher(bearerToken);
         if (!matcher.find()) {
+
             throw new GeneralException(LoginException.INVALID_BEARER_TOKEN_FORMAT);
         }
 
