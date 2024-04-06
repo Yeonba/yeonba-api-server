535f7af1851f6b75c2a385af83d9d05e9957c013
Minjae-An <mj3242@naver.com>
2024-04-06 20:29:43 +0900

    refactor: 사용자 로그인 응답 DTO 수정(#50)
    - refresh token을 쿠키를 통해 전달함에 따라 관련 필드 삭제
    - 사용자 휴면 여부 필드 추가

--- be/src/main/java/yeonba/be/login/dto/response/UserLoginResponse.java
+++ be/src/main/java/yeonba/be/login/dto/response/UserLoginResponse.java
@@ -9,21 +9,18 @@ import lombok.Getter;
 @AllArgsConstructor
 public class UserLoginResponse {
 
-	@Schema(
-		type = "string",
-		description = "access token",
-		example = """
-			eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9
-			.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ
-			.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c""")
-	private String accessToken;
+    @Schema(
+        type = "string",
+        description = "access token",
+        example = """
+            eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9
+            .eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ
+            .SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c""")
+    private String accessToken;
 
-	@Schema(
-		type = "string",
-		description = "refresh token",
-		example = """
-			eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9
-			.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ
-			.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c""")
-	private String refreshToken;
+    @Schema(
+        type = "boolean",
+        description = "사용자 휴면 여부, true일 경우 휴면 해제 필요",
+        example = "true")
+    private Boolean isInactiveUser;
 }
