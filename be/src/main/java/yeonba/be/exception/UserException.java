4ab2943902c17dee79f93a747a2157fdb7cf76b3
Minjae-An <mj3242@naver.com>
2024-04-06 14:32:28 +0900

    refactor: 코드 정렬(#57)

--- be/src/main/java/yeonba/be/exception/UserException.java
+++ be/src/main/java/yeonba/be/exception/UserException.java
@@ -4,25 +4,28 @@ import org.springframework.http.HttpStatus;
 
 public enum UserException implements BaseException {
 
-  USER_NOT_FOUND(
-      HttpStatus.BAD_REQUEST,
-      "해당 사용자가 존재하지 않습니다.");
+    USER_NOT_FOUND(
+        HttpStatus.BAD_REQUEST,
+        "해당 사용자가 존재하지 않습니다.");
 
-  private final HttpStatus httpStatus;
-  private final String reason;
+    private final HttpStatus httpStatus;
+    private final String reason;
 
-  UserException(HttpStatus httpStatus, String reason) {
-    this.httpStatus = httpStatus;
-    this.reason = reason;
-  }
+    UserException(HttpStatus httpStatus, String reason) {
 
-  @Override
-  public HttpStatus getHttpStatus() {
-    return httpStatus;
-  }
+        this.httpStatus = httpStatus;
+        this.reason = reason;
+    }
 
-  @Override
-  public String getReason() {
-    return reason;
-  }
+    @Override
+    public HttpStatus getHttpStatus() {
+
+        return httpStatus;
+    }
+
+    @Override
+    public String getReason() {
+
+        return reason;
+    }
 }
