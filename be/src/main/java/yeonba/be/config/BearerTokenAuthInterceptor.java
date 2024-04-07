2b469a2d1ba96af406b9fe08777450f56d24e4ae
Minjae-An <mj3242@naver.com>
2024-04-07 14:56:14 +0900

    refactor: 불필요한 주석 제거(#50)

--- be/src/main/java/yeonba/be/config/BearerTokenAuthInterceptor.java
+++ be/src/main/java/yeonba/be/config/BearerTokenAuthInterceptor.java
@@ -19,13 +19,6 @@ public class BearerTokenAuthInterceptor implements HandlerInterceptor {
 
     private final JwtUtil jwtUtil;
 
-    /*
-    access token을 이용한 인가는 이하 과정을 거쳐 이뤄진다.
-    1. Authorization 헤더 값이 존재하는 지 확인
-    2. Bearer 토큰 형식이 맞는 지 확인
-    4. access token 파싱, jwt 검증 및 userId 파싱
-    5. userId request attribute 설정
-     */
     @Override
     public boolean preHandle(
         HttpServletRequest request,
@@ -35,7 +28,6 @@ public class BearerTokenAuthInterceptor implements HandlerInterceptor {
         // authorization 헤더 값 존재여부 확인
         String bearerToken = request.getHeader("Authorization");
         if (!StringUtils.hasText(bearerToken)) {
-
             throw new GeneralException(LoginException.ACCESS_TOKEN_NOT_EXIST);
         }
 
@@ -43,7 +35,6 @@ public class BearerTokenAuthInterceptor implements HandlerInterceptor {
         String regex = GlobalValidationRegex.BEARER_TOKEN.getPattern();
         Matcher matcher = Pattern.compile(regex).matcher(bearerToken);
         if (!matcher.find()) {
-
             throw new GeneralException(LoginException.INVALID_BEARER_TOKEN_FORMAT);
         }
 
