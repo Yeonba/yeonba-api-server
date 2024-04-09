554eeafaae3530b5f8db967c11faddd53a8ee729
luke <gseonggyu968@gmail.com>
2024-04-09 11:41:56 +0900

    fix: 스웨거에서 request 시 cors 에러 수정

--- be/src/main/java/yeonba/be/config/SwaggerConfig.java
+++ be/src/main/java/yeonba/be/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package yeonba.be.config;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,6 +21,7 @@ public class SwaggerConfig {
 
         return new OpenAPI()
             .components(new Components())
+            .addServersItem(new Server().url("https://api.yeonba.co.kr"))
             .info(info);
     }
 
