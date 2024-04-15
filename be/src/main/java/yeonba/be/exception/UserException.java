b7729805aa37f0d647c329b153236b20d497ab0a
Minjae-An <mj3242@naver.com>
2024-04-15 21:12:50 +0900

    feat: 프로필 수정 관련 예외 추가(#38)
    - 성인이 아닌 경우
    - 하한 값이 상한 값보다 큰 경우

--- be/src/main/java/yeonba/be/exception/UserException.java
+++ be/src/main/java/yeonba/be/exception/UserException.java
@@ -4,25 +4,40 @@ import org.springframework.http.HttpStatus;
 
 public enum UserException implements BaseException {
 
-  USER_NOT_FOUND(
-      HttpStatus.BAD_REQUEST,
-      "해당 사용자가 존재하지 않습니다.");
+    USER_NOT_FOUND(
+        HttpStatus.BAD_REQUEST,
+        "해당 사용자가 존재하지 않습니다."),
 
-  private final HttpStatus httpStatus;
-  private final String reason;
+    USER_PREFERENCE_NOT_FOUND(
+        HttpStatus.BAD_REQUEST,
+        "해당 사용자의 선호내역이 존재하지 않습니다."),
 
-  UserException(HttpStatus httpStatus, String reason) {
-    this.httpStatus = httpStatus;
-    this.reason = reason;
-  }
+    IS_NOT_ADULT(
+        HttpStatus.BAD_REQUEST,
+        "만 18세 미만은 저희 서비스를 이용할 수 없습니다."),
 
-  @Override
-  public HttpStatus getHttpStatus() {
-    return httpStatus;
-  }
+    LOWER_BOUND_LESS_THAN_OR_EQUAL_UPPER_BOUND(
+        HttpStatus.BAD_REQUEST,
+        "하한 값은 상한 값보다 작거나 같아야 합니다.");
 
-  @Override
-  public String getReason() {
-    return reason;
-  }
+    private final HttpStatus httpStatus;
+    private final String reason;
+
+    UserException(HttpStatus httpStatus, String reason) {
+
+        this.httpStatus = httpStatus;
+        this.reason = reason;
+    }
+
+    @Override
+    public HttpStatus getHttpStatus() {
+
+        return httpStatus;
+    }
+
+    @Override
+    public String getReason() {
+
+        return reason;
+    }
 }
