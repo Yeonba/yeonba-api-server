8aca7d3dfcdfb39cb0002571b2ee11f2ade75f9c
Minjae-An <mj3242@naver.com>
2024-04-15 21:05:40 +0900

    feat: 서비스 정규식 enum 정리(#38)
    병합 과정에서 잘못 설정된 enum 통합

--- be/src/main/java/yeonba/be/util/GlobalValidationRegex.java
+++ be/src/main/java/yeonba/be/util/GlobalValidationRegex.java
@@ -3,7 +3,9 @@ package yeonba.be.util;
 public enum GlobalValidationRegex {
 
 	EMAIL("[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$"),
-	PASSWORD("^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[~#@!]).{8,20}$");
+	PASSWORD("^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[~#@!]).{8,20}$"),
+	VERIFICATION_CODE("^[A-Za-z0-9]{6}$"),
+	PHONE_NUMBER("^010\\d{8}$");
 
 	private final String pattern;
 
