9fb9811c0f4a399d58d57fc9016332da1eb42a1d
Minjae-An <mj3242@naver.com>
2024-04-19 21:15:41 +0900

    feat: 받은 알림 목록 조회 비즈니스 로직 추가(#66)
    페이지 형태로 알림 목록 제공, 페이지 사이즈는 고정 9

--- be/src/main/java/yeonba/be/notification/service/NotificationService.java
+++ be/src/main/java/yeonba/be/notification/service/NotificationService.java
@@ -1,8 +1,13 @@
 package yeonba.be.notification.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import yeonba.be.notification.dto.request.NotificationReceivedRequest;
+import yeonba.be.notification.dto.response.NotificationPageResponse;
+import yeonba.be.notification.dto.response.NotificationResponse;
 import yeonba.be.notification.dto.response.NotificationUnreadCountResponse;
 import yeonba.be.notification.repository.NotificationQuery;
 import yeonba.be.user.entity.User;
@@ -22,5 +27,20 @@ public class NotificationService {
         long unreadNotificationsCount = notificationQuery.countUnreadNotificationsBy(receiver);
 
         return new NotificationUnreadCountResponse(unreadNotificationsCount);
+    }
+
+    @Transactional(readOnly = true)
+    public NotificationPageResponse getReceivedNotificationsBy(
+        long receiverId, NotificationReceivedRequest request) {
+
+        int pageNumber = request.getPage();
+        int size = 9;
+
+        PageRequest pageRequest = PageRequest.of(pageNumber, size);
+
+        Page<NotificationResponse> page =
+            notificationQuery.findReceivedNotificationsBy(receiverId, pageRequest);
+
+        return NotificationPageResponse.of(page);
     }
 }
