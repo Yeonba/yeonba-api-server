5726304d287f93a4f3301363701168a2c5c17d6b
Minjae-An <mj3242@naver.com>
2024-04-20 16:59:43 +0900

    feat: 사용자 알림 동의 내역 수정 요청 DTO 수정(#66)
    - 요청한 채팅 수락 시 알림 동의 여부 필드 추가
    - not null 제약 조건 추가

--- be/src/main/java/yeonba/be/mypage/dto/request/UserAllowNotificationsRequest.java
+++ be/src/main/java/yeonba/be/mypage/dto/request/UserAllowNotificationsRequest.java
@@ -1,6 +1,7 @@
 package yeonba.be.mypage.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,16 +11,24 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class UserAllowNotificationsRequest {
 
-  @Schema(
-      type = "boolean",
-      description = "화살 받을 시 알림 허용 여부",
-      example = "true"
-  )
-  private Boolean allowReceivedArrowNotification;
-  @Schema(
-      type = "boolean",
-      description = "채팅 요청 받을 시 알림 허용 여부",
-      example = "true"
-  )
-  private Boolean allowChattingRequestNotification;
+    @Schema(
+        type = "boolean",
+        description = "화살 받을 시 알림 동의 여부",
+        example = "true")
+    @NotNull(message = "화살 받을 시 알림 동의 여부는 필수 값입니다.")
+    private Boolean allowArrowReceivedNotification;
+
+    @Schema(
+        type = "boolean",
+        description = "채팅 요청 받을 시 알림 동의 여부",
+        example = "true")
+    @NotNull(message = "채팅 요청 받을 시 알림 동의 여부는 필수 값입니다.")
+    private Boolean allowChattingRequestNotification;
+
+    @Schema(
+        type = "boolean",
+        description = "요청한 채팅 수락 시 알림 동의 여부",
+        example = "true")
+    @NotNull(message = "요청한 채팅 수락 시 알림 동의 여부는 필수 값입니다.")
+    private Boolean allowChattingRequestAcceptedNotification;
 }
