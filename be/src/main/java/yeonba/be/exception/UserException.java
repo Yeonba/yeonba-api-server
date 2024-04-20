65aac96da925c0976aa1b9425f31744b072ef0da
Minjae-An <mj3242@naver.com>
2024-04-20 20:52:22 +0900

    feat: 사용자 연관 예외 추가(#68)
    - 같은 사용자(자기 자신)일 경우
    - 같은 성별 사용자인 경우
    - 휴면 상태 사용자인 경우

--- be/src/main/java/yeonba/be/exception/UserException.java
+++ be/src/main/java/yeonba/be/exception/UserException.java
@@ -1,28 +1,29 @@
 package yeonba.be.exception;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+@Getter
+@AllArgsConstructor
 public enum UserException implements BaseException {
 
-  USER_NOT_FOUND(
-      HttpStatus.BAD_REQUEST,
-      "해당 사용자가 존재하지 않습니다.");
+    SAME_USER(
+        HttpStatus.BAD_REQUEST,
+        "같은 사용자(자기 자신) 입니다."),
 
-  private final HttpStatus httpStatus;
-  private final String reason;
+    SAME_GENDER_USER(
+        HttpStatus.BAD_REQUEST,
+        "같은 성별 사용자 입니다."),
 
-  UserException(HttpStatus httpStatus, String reason) {
-    this.httpStatus = httpStatus;
-    this.reason = reason;
-  }
+    USER_NOT_FOUND(
+        HttpStatus.BAD_REQUEST,
+        "해당 사용자가 존재하지 않습니다."),
 
-  @Override
-  public HttpStatus getHttpStatus() {
-    return httpStatus;
-  }
+    INACTIVE_USER(
+        HttpStatus.BAD_REQUEST,
+        "휴면 상태 사용자입니다. 휴면 해제가 필요합니다.");
 
-  @Override
-  public String getReason() {
-    return reason;
-  }
+    private final HttpStatus httpStatus;
+    private final String reason;
 }
