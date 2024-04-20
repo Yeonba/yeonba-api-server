a57eb4eac95f998a831afb03b1094494e3abbd74
Minjae-An <mj3242@naver.com>
2024-04-20 20:54:22 +0900

    feat: 휴면 상태 사용자에게 화살 보냄 예외 정의(#68)

--- be/src/main/java/yeonba/be/exception/ArrowException.java
+++ be/src/main/java/yeonba/be/exception/ArrowException.java
@@ -1,7 +1,11 @@
 package yeonba.be.exception;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+@Getter
+@AllArgsConstructor
 public enum ArrowException implements BaseException {
 
 
@@ -19,25 +23,12 @@ public enum ArrowException implements BaseException {
 
     NOT_ENOUGH_ARROW_TO_SEND(
         HttpStatus.BAD_REQUEST,
-        "화살이 부족하여 화살을 보낼 수 없습니다.");
+        "화살이 부족하여 화살을 보낼 수 없습니다."),
+
+    CAN_NOT_SEND_ARROW_TO_INACTIVE_USER(
+        HttpStatus.BAD_REQUEST,
+        "휴면 상태인 사용자에겐 화살을 보낼 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String reason;
-
-    ArrowException(HttpStatus httpStatus, String reason) {
-        this.httpStatus = httpStatus;
-        this.reason = reason;
-    }
-
-    @Override
-    public HttpStatus getHttpStatus() {
-
-        return httpStatus;
-    }
-
-    @Override
-    public String getReason() {
-
-        return reason;
-    }
 }
