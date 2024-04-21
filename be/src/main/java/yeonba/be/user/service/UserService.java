b1d29b3d717e1f58689368add7f605ebc5f5f895
Minjae-An <mj3242@naver.com>
2024-04-21 15:25:46 +0900

    feat: 최종 접속 일시 갱신 로직 추가(#68)
    출석 체크 요청시 무조건적인 최종 접속 일시 갱신을 위해 사용

--- be/src/main/java/yeonba/be/user/service/UserService.java
+++ be/src/main/java/yeonba/be/user/service/UserService.java
@@ -1,6 +1,7 @@
 package yeonba.be.user.service;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.Period;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -173,5 +174,12 @@ public class UserService {
             preferredArea,
             preferredAnimal);
         userPreferenceCommand.save(userPreference);
+    }
+
+    @Transactional
+    public void updateLastAccessedAt(long userId, LocalDateTime accessAt) {
+
+        User user = userQuery.findById(userId);
+        user.updateLastAccessedAt(accessAt);
     }
 }
