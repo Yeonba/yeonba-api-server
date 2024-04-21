3c34858c725d901e6b36be2edb749c9ae1f47443
Minjae-An <mj3242@naver.com>
2024-04-21 15:26:26 +0900

    feat: 인증되지 않은 사용자 예외 추가(#68)

--- be/src/main/java/yeonba/be/exception/LoginException.java
+++ be/src/main/java/yeonba/be/exception/LoginException.java
@@ -1,31 +1,21 @@
 package yeonba.be.exception;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+@Getter
+@AllArgsConstructor
 public enum LoginException implements BaseException {
 
     VERIFICATION_CODE_NOT_FOUND(
         HttpStatus.BAD_REQUEST,
-        "해당 인증 코드 내역이 존재하지 않습니다.");
+        "해당 인증 코드 내역이 존재하지 않습니다."),
+
+    UNAUTHORIZED(
+        HttpStatus.UNAUTHORIZED,
+        "인증되지 않은 사용자입니다. 로그인이 필요합니다.");
 
     private final HttpStatus httpStatus;
     private final String reason;
-
-    LoginException(HttpStatus httpStatus, String reason) {
-
-        this.httpStatus = httpStatus;
-        this.reason = reason;
-    }
-
-    @Override
-    public HttpStatus getHttpStatus() {
-
-        return httpStatus;
-    }
-
-    @Override
-    public String getReason() {
-
-        return reason;
-    }
 }
