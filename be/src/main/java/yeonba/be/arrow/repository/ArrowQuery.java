f66740df7e36f44cb79eeabd4dc84e1e9e8275c1
Minjae-An <mj3242@naver.com>
2024-04-22 22:02:19 +0900

    feat: 메서드 파라미터명 수정(#68)

--- be/src/main/java/yeonba/be/arrow/repository/ArrowQuery.java
+++ be/src/main/java/yeonba/be/arrow/repository/ArrowQuery.java
@@ -13,10 +13,10 @@ public class ArrowQuery {
 
     private final ArrowTransactionRepository arrowTransactionRepository;
 
-    public boolean isArrowTransactionExist(User sentUser, User receivedUser) {
+    public boolean isArrowTransactionExist(User sender, User receiver) {
 
         return arrowTransactionRepository
-            .existsBySenderAndReceiver(sentUser, receivedUser);
+            .existsBySenderAndReceiver(sender, receiver);
     }
 
     public void validateAdvertisementArrowCount(long userId, LocalDateTime today) {
