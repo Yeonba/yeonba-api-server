82cd65fddb1cbc80fc384f68a6265cd75f26de4d
Minjae-An <mj3242@naver.com>
2024-04-30 21:41:25 +0900

    feat: 프로필 업데이트 로직, 업데이트 가능한 필드 추가(#38)
    업데이트 가능한 필드 추가

--- be/src/main/java/yeonba/be/user/entity/User.java
+++ be/src/main/java/yeonba/be/user/entity/User.java
@@ -231,14 +231,24 @@ public class User {
     }
 
     public void updateProfile(
+        String nickname,
+        int height,
         LocalDate birth,
         int age,
+        String bodyType,
+        String job,
+        String mbti,
         VocalRange vocalRange,
         Animal animal,
         Area area) {
 
+        this.nickname = nickname;
+        this.height = height;
         this.birth = birth;
         this.age = age;
+        this.bodyType = bodyType;
+        this.job = job;
+        this.mbti = mbti;
         this.vocalRange = vocalRange;
         this.animal = animal;
         this.area = area;
