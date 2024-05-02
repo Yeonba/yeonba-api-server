7b7e09f6c365ebba6d31478d1a2dad824e2b338d
Minjae-An <mj3242@naver.com>
2024-05-02 22:02:27 +0900

    feat: 사용자가 받은 알림 목록 조회 로직 추가(#66)

--- be/src/main/java/yeonba/be/notification/repository/NotificationRepository.java
+++ be/src/main/java/yeonba/be/notification/repository/NotificationRepository.java
@@ -1,15 +1,18 @@
 package yeonba.be.notification.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import yeonba.be.notification.entity.Notification;
 import yeonba.be.user.entity.User;
 
 @Repository
-public interface NotificationRepository
-    extends JpaRepository<Notification, Long>, NotificationRepositoryCustom {
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
     long countByReceiverAndReadIsFalse(User receiver);
 
     boolean existsByReceiverAndReadIsFalse(User receiver);
+
+    Page<Notification> findAllByReceiver(User receiver, PageRequest pageRequest);
 }
