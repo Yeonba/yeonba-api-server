84d032443d78760ecccb43362d57208abe278e36
Minjae-An <mj3242@naver.com>
2024-05-04 21:14:09 +0900

    feat: 불필요한 FCM 예외 삭제(#66)

--- be/src/main/java/yeonba/be/exception/UtilException.java
+++ be/src/main/java/yeonba/be/exception/UtilException.java
@@ -14,11 +14,7 @@ public enum UtilException implements BaseException {
 
     NOT_ALLOWED_IMAGE_FILE_EXTENSION(
         HttpStatus.BAD_REQUEST,
-        "jpg, jpeg, png 확장자 형식의 파일만 허용됩니다."),
-
-    FCM_EXCEPTION(
-        HttpStatus.INTERNAL_SERVER_ERROR,
-        "FCM을 통한 알림 전송 도중 예외가 발생하였습니다. 관리자에게 문의하세요");
+        "jpg, jpeg, png 확장자 형식의 파일만 허용됩니다.");
 
     private final HttpStatus httpStatus;
     private final String reason;
