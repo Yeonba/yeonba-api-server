b9690d61d3d2f5d1e2fcccad4dde39c540181b8c
Minjae-An <mj3242@naver.com>
2024-05-05 20:19:22 +0900

    feat: 알림 동의 내역 조회 로직 추가(#66)

--- be/src/main/java/yeonba/be/notification/repository/NotificationPermissionQuery.java
+++ be/src/main/java/yeonba/be/notification/repository/NotificationPermissionQuery.java
@@ -3,7 +3,10 @@ package yeonba.be.notification.repository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import yeonba.be.exception.GeneralException;
+import yeonba.be.exception.NotificationException;
 import yeonba.be.notification.entity.NotificationPermission;
+import yeonba.be.notification.entity.NotificationType;
 import yeonba.be.user.entity.User;
 
 @Component
@@ -15,5 +18,12 @@ public class NotificationPermissionQuery {
     public List<NotificationPermission> findAllByUser(User user) {
 
         return notificationPermissionRepository.findAllByUser(user);
+    }
+
+    public NotificationPermission findByUserAndType(User user, NotificationType type) {
+
+        return notificationPermissionRepository.findByUserAndType(user, type)
+            .orElseThrow(() ->
+                new GeneralException(NotificationException.NOTIFICATION_PERMISSION_NOT_FOUND));
     }
 }
