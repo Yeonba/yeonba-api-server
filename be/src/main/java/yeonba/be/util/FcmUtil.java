d490a809692831e480bab1e86f3c09d72aa6bb93
Minjae-An <mj3242@naver.com>
2024-05-05 17:07:07 +0900

    feat: 알림 전송 로직 변경(#66)
    - Apns를 이용할 때 보다 구체화된 설정을 제공하는 ApnsConfig를 이용하도록 로직 수정
    - 알림에 badge 포함, badge의 의미는 알림 수신자가 읽지 않은 알림 개수
    - 알림에 이미지 포함하는 로직 삭제
    - fcm 이용 도중 발생한 장애가 비즈니스 로직에까지 전파되는 형태는 부적절하다고 판단
    - 다만, 외부 서비스와 관련하여 발생한 장애를 클라이언트에게 전달할 필요는 없다고 생각
    - 서버 내부에서 관련 예외를 로그로 남겨, 장애 발생시 대응할 수 있는 형태로 예외 처리 구성

--- be/src/main/java/yeonba/be/util/FcmUtil.java
+++ be/src/main/java/yeonba/be/util/FcmUtil.java
@@ -1,13 +1,14 @@
 package yeonba.be.util;
 
+import com.google.firebase.messaging.ApnsConfig;
+import com.google.firebase.messaging.Aps;
+import com.google.firebase.messaging.ApsAlert;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
-import com.google.firebase.messaging.Notification;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import yeonba.be.exception.GeneralException;
@@ -19,28 +20,32 @@ import yeonba.be.notification.event.NotificationSendEvent;
 @Slf4j
 public class FcmUtil {
 
-    @Value("${NOTIFICATION_ICON_URL}")
-    private String notificationIconUrl;
-
     private final FirebaseMessaging firebaseMessaging;
 
     @Async
-    public void sendNotification(NotificationSendEvent sendEvent) {
+    public void sendNotification(NotificationSendEvent sendEvent, int badge) {
 
         String deviceToken = Optional.ofNullable(sendEvent.receiver().getDeviceToken())
             .orElseThrow(() -> new GeneralException(NotificationException.DEVICE_TOKEN_NOT_FOUND));
 
-        Notification notification = Notification.builder()
-            .setImage(notificationIconUrl)
-            .setTitle(sendEvent.getNotificationTitle())
-            .setBody(sendEvent.getNotificationMessage())
+        ApnsConfig apnsConfig = ApnsConfig.builder()
+            .setAps(
+                Aps.builder().setAlert(
+                        ApsAlert.builder()
+                            .setTitle(sendEvent.getNotificationTitle())
+                            .setBody(sendEvent.getNotificationMessage())
+                            .build()
+                    )
+                    .setBadge(badge)
+                    .putCustomData("creatorId", sendEvent.creator().getId())
+                    .putCustomData("createdAt", sendEvent.createdAt().toString())
+                    .build()
+            )
             .build();
 
         Message message = Message.builder()
             .setToken(deviceToken)
-            .setNotification(notification)
-            .putData("creatorId", String.valueOf(sendEvent.creator().getId()))
-            .putData("createdAt", String.valueOf(sendEvent.createdAt()))
+            .setApnsConfig(apnsConfig)
             .build();
 
         try {
