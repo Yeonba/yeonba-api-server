716155dd89595257517dd001b31808b2becfd888
Minjae-An <mj3242@naver.com>
2024-05-05 16:52:42 +0900

    feat: 채팅 관련 상수 수정, 알림 메시지 제공 메서드 추가(#66)

--- be/src/main/java/yeonba/be/notification/entity/NotificationType.java
+++ be/src/main/java/yeonba/be/notification/entity/NotificationType.java
@@ -8,10 +8,14 @@ import lombok.Getter;
 public enum NotificationType {
 
     ARROW_RECEIVED("화살 수신", "%s님이 화살을 보냈어요!"),
-    CHAT_REQUESTED("채팅 요청 수신", "%s님이 채팅을 요청했어요!"),
-    CHAT_REQUEST_ACCEPTED("요청한 채팅 수락됨", "%s님이 채팅을 수락했어요!");
+    CHATTING_REQUESTED("채팅 요청 수신", "%s님이 채팅을 요청했어요!"),
+    CHATTING_REQUEST_ACCEPTED("요청한 채팅 수락됨", "%s님이 채팅을 수락했어요!");
 
     private final String title;
     private final String message;
 
+    public String getFormattedMessage(String username) {
+
+        return String.format(this.message, username);
+    }
 }
