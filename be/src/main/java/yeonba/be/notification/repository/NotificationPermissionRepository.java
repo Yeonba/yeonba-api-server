7ffc527014178afb09c83455637ed2107490b983
Minjae-An <mj3242@naver.com>
2024-05-05 18:12:33 +0900

    feat: 알림 동의 내역 목록 조회 메서드 추가(#66)
    - 불필요한 조회 메서드 삭제
    - 알림 동의 내역 목록 조회 메서드 추가

--- be/src/main/java/yeonba/be/notification/repository/NotificationPermissionRepository.java
+++ be/src/main/java/yeonba/be/notification/repository/NotificationPermissionRepository.java
@@ -1,15 +1,14 @@
 package yeonba.be.notification.repository;
 
-import java.util.Optional;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import yeonba.be.notification.entity.NotificationPermission;
-import yeonba.be.notification.entity.NotificationType;
 import yeonba.be.user.entity.User;
 
 @Repository
 public interface NotificationPermissionRepository
     extends JpaRepository<NotificationPermission, Long> {
 
-    Optional<NotificationPermission> findByUserAndType(User user, NotificationType type);
+    List<NotificationPermission> findAllByUser(User user);
 }
