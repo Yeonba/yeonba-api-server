b594e4ae6514e0f3d4ac4a93b776f69c88a35ee8
Minjae-An <mj3242@naver.com>
2024-05-05 20:20:58 +0900

    feat: 사용자 알림 동의 여부 확인 로직 추가(#66)
    특정 타입의 알림을 사용자가 동의했을 경우만 푸시 알림으로 전달하도록 로직 추가

--- be/src/main/java/yeonba/be/notification/event/NotificationEventListener.java
+++ be/src/main/java/yeonba/be/notification/event/NotificationEventListener.java
@@ -16,8 +16,12 @@ public class NotificationEventListener {
     @EventListener
     public void sendNotification(NotificationSendEvent sendEvent) {
 
-        int badge = notificationService.getBadge(sendEvent.receiver().getId());
-        fcmUtil.sendNotification(sendEvent, badge);
+        // 사용자가 알림 수신에 동의했을 때만 푸시 알림 전송
+        if (notificationService.isUserAllowedNotification(sendEvent.receiver(), sendEvent.type())) {
+            int badge = notificationService.getBadge(sendEvent.receiver().getId());
+            fcmUtil.sendNotification(sendEvent, badge);
+        }
+
         notificationService.saveNotification(sendEvent);
     }
 }
