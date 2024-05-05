b4c5713924d261a28ffef5e6385e1174bfbee32a
Minjae-An <mj3242@naver.com>
2024-05-05 16:54:24 +0900

    feat: 필드 타입, 기본 타입으로 변경(#66)

--- be/src/main/java/yeonba/be/mypage/dto/request/UserAllowNotificationsRequest.java
+++ be/src/main/java/yeonba/be/mypage/dto/request/UserAllowNotificationsRequest.java
@@ -2,13 +2,11 @@ package yeonba.be.mypage.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
 public class UserAllowNotificationsRequest {
 
     @Schema(
@@ -16,19 +14,19 @@ public class UserAllowNotificationsRequest {
         description = "화살 받을 시 알림 동의 여부",
         example = "true")
     @NotNull(message = "화살 받을 시 알림 동의 여부는 필수 값입니다.")
-    private Boolean allowArrowReceivedNotification;
+    private boolean allowArrowReceivedNotification;
 
     @Schema(
         type = "boolean",
         description = "채팅 요청 받을 시 알림 동의 여부",
         example = "true")
     @NotNull(message = "채팅 요청 받을 시 알림 동의 여부는 필수 값입니다.")
-    private Boolean allowChattingRequestNotification;
+    private boolean allowChattingRequestNotification;
 
     @Schema(
         type = "boolean",
         description = "요청한 채팅 수락 시 알림 동의 여부",
         example = "true")
     @NotNull(message = "요청한 채팅 수락 시 알림 동의 여부는 필수 값입니다.")
-    private Boolean allowChattingRequestAcceptedNotification;
+    private boolean allowChattingRequestAcceptedNotification;
 }
