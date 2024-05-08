93dda5d413fe9dce71a3b0dc78dc905bc6d170b3
Minjae-An <mj3242@naver.com>
2024-05-08 21:40:12 +0900

    feat: 사용 중인 닉네임 검증 API 추가(#78)

--- be/src/main/java/yeonba/be/login/controller/LoginController.java
+++ be/src/main/java/yeonba/be/login/controller/LoginController.java
@@ -5,7 +5,9 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,9 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 import yeonba.be.login.dto.request.UserJoinRequest;
 import yeonba.be.login.dto.request.UserLoginRequest;
 import yeonba.be.login.dto.request.UserRefreshJwtRequest;
+import yeonba.be.login.dto.request.UserValidateUsedNicknameRequest;
 import yeonba.be.login.dto.response.UserJoinResponse;
 import yeonba.be.login.dto.response.UserLoginResponse;
 import yeonba.be.login.dto.response.UserRefrehJwtResponse;
+import yeonba.be.login.dto.response.UserValidateUsedNicknameResponse;
 import yeonba.be.login.service.LoginService;
 import yeonba.be.user.service.JoinService;
 import yeonba.be.util.CustomResponse;
@@ -61,6 +65,20 @@ public class LoginController {
         @RequestBody UserRefreshJwtRequest request) {
 
         UserRefrehJwtResponse response = loginService.refreshJwt(request);
+
+        return ResponseEntity
+            .ok()
+            .body(new CustomResponse<>(response));
+    }
+
+    @Operation(summary = "사용 중인 닉네임 확인", description = "사용 중인 닉네임인 지 검증할 수 있습니다.")
+    @ApiResponse(responseCode = "200", description = "닉네임 중복 검증 성공")
+    @GetMapping("/users/nicknames/used")
+    public ResponseEntity<CustomResponse<UserValidateUsedNicknameResponse>> validateUsedNickname(
+        @Valid @ParameterObject UserValidateUsedNicknameRequest request) {
+
+        UserValidateUsedNicknameResponse response =
+            loginService.validateUsedNickname(request);
 
         return ResponseEntity
             .ok()
