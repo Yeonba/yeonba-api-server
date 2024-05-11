087130b06737d171ed90d5fa2c84092edb5ad0ca
Minjae-An <mj3242@naver.com>
2024-05-11 19:19:55 +0900

    feat: 알림 읽음 처리 메서드 수정(#66)
    - 기존 로직은 주어진 ID보다 작거나 같은 ID를 가진 알림을 전부 읽음 처리
    - 모든 사용자가 받은 알림이 읽음 처리되는 잘못된 로직
    - 특정 사용자가 받은 알림만 ID를 기준으로 알림 처리할 수 있도록 로직 수정
    - 메서드명 변경

--- be/src/main/java/yeonba/be/notification/repository/NotificationRepositoryImpl.java
+++ be/src/main/java/yeonba/be/notification/repository/NotificationRepositoryImpl.java
@@ -13,11 +13,13 @@ public class NotificationRepositoryImpl implements NotificationRepositoryCustom 
     private final JPAQueryFactory queryFactory;
 
     @Override
-    public void readAllHasIdLessThanEqual(long notificationId) {
+    public void readNotificationsUpToIdBy(long receiverId, long notificationId) {
 
         queryFactory.update(notification)
             .set(notification.read, true)
-            .where(notification.id.loe(notificationId))
+            .where(
+                notification.receiver.id.eq(receiverId),
+                notification.id.loe(notificationId))
             .execute();
 
         // 벌크성 수정 쿼리는 영속성 컨텍스트를 무시하기 때문에, 실행 후 컨텍스트 초기화 수행
