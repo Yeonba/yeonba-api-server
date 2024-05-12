5321d6a6655bc9fccb0ee2c35ff6dfff786629de
Minjae-An <mj3242@naver.com>
2024-05-12 17:40:28 +0900

    feat: 불필요하게 추가되었던 휴면 해제 관련 로직 삭제(#57)

--- be/src/main/java/yeonba/be/user/controller/UserController.java
+++ be/src/main/java/yeonba/be/user/controller/UserController.java
@@ -9,7 +9,6 @@ import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
@@ -127,19 +126,6 @@ public class UserController {
         @PathVariable("userId") long blockedUserId) {
 
         blockService.block(userId, blockedUserId);
-
-        return ResponseEntity
-            .accepted()
-            .body(new CustomResponse<>());
-    }
-
-    @Operation(summary = "휴면 해제", description = "사용자 휴면 상태 해제")
-    @ApiResponse(responseCode = "200", description = "휴면 해제 정상 처리")
-    @PatchMapping("/users/activate")
-    public ResponseEntity<CustomResponse<Void>> activateUser(
-        @RequestAttribute("userId") long userId) {
-
-        userService.activateUser(userId);
 
         return ResponseEntity
             .accepted()
