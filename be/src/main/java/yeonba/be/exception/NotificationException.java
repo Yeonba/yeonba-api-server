752184f13a08a31a1e8b85d0c9f988acc9042021
Minjae-An <mj3242@naver.com>
2024-05-13 16:49:33 +0900

    feat: 요청 동의 여부에 null 값이 포함되는 예외 정의(#66)

--- be/src/main/java/yeonba/be/exception/NotificationException.java
+++ be/src/main/java/yeonba/be/exception/NotificationException.java
@@ -14,7 +14,11 @@ public enum NotificationException implements BaseException {
 
     DEVICE_TOKEN_NOT_FOUND(
         HttpStatus.BAD_REQUEST,
-        "해당 사용자의 device token이 존재하지 않습니다. token을 먼저 등록해주세요.");
+        "해당 사용자의 device token이 존재하지 않습니다. token을 먼저 등록해주세요."),
+
+    REQUEST_PERMISSIONS_CAN_NOT_CONTAIN_NULL(
+        HttpStatus.BAD_REQUEST,
+        "요청되는 동의 여부엔 null 값이 포함될 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String reason;
