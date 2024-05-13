a8925cf5701b69bfd77ad2e8f42eba5f56608ff8
Minjae-An <mj3242@naver.com>
2024-05-13 21:10:48 +0900

    feat: 사용하지 않는 예외 삭제(#68)

--- be/src/main/java/yeonba/be/exception/ArrowException.java
+++ be/src/main/java/yeonba/be/exception/ArrowException.java
@@ -13,10 +13,6 @@ public enum ArrowException implements BaseException {
         HttpStatus.BAD_REQUEST,
         "1일 광고 시청은 최대 3회입니다."),
 
-    ALREADY_CHECKED_USER(
-        HttpStatus.BAD_REQUEST,
-        "이미 출석 체크한 사용자입니다."),
-
     ALREADY_SENT_ARROW_USER(
         HttpStatus.BAD_REQUEST,
         "이미 화살을 보낸 사용자입니다."),
