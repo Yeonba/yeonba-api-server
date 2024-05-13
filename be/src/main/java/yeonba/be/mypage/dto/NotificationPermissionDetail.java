cc7c687b7fa5482feacea1586712443f3574e444
Minjae-An <mj3242@naver.com>
2024-05-13 18:40:17 +0900

    feat: 필드 검증 추가(#66)

--- be/src/main/java/yeonba/be/mypage/dto/NotificationPermissionDetail.java
+++ be/src/main/java/yeonba/be/mypage/dto/NotificationPermissionDetail.java
@@ -2,11 +2,12 @@ package yeonba.be.mypage.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import yeonba.be.notification.entity.NotificationPermission;
-import yeonba.be.notification.enums.NotificationType;
 
 @Getter
 @NoArgsConstructor
@@ -17,19 +18,24 @@ public class NotificationPermissionDetail {
         type = "string",
         description = "알림 유형",
         example = "ARROW_RECEIVED")
-    private NotificationType type;
+    @Pattern(
+        regexp = "^(ARROW_RECEIVED|CHATTING_REQUESTED|CHATTING_REQUEST_ACCEPTED)$",
+        message = "알림 유형은 허용된 문자열만 가능합니다.")
+    @NotNull(message = "알림 유형은 반드시 입력되어야 합니다.")
+    private String type;
 
     @Schema(
         type = "boolean",
         description = "알림 동의 여부",
         example = "true")
     @JsonProperty("isPermit")
+    @NotNull(message = "알림 동의 여부는 반드시 입력되어야 합니다.")
     private boolean permit;
 
     public static NotificationPermissionDetail of(NotificationPermission notificationPermission) {
 
         return new NotificationPermissionDetail(
-            notificationPermission.getType(),
+            notificationPermission.getType().name(),
             notificationPermission.getPermissionStatus());
     }
 }
