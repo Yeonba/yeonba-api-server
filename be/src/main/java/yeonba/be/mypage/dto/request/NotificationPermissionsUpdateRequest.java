93495dc71170e2f4aed77bf0ac6a9d0d7977cb12
Minjae-An <mj3242@naver.com>
2024-05-13 18:40:32 +0900

    feat: 필드 검증 추가(#66)

--- be/src/main/java/yeonba/be/mypage/dto/request/NotificationPermissionsUpdateRequest.java
+++ be/src/main/java/yeonba/be/mypage/dto/request/NotificationPermissionsUpdateRequest.java
@@ -1,6 +1,7 @@
 package yeonba.be.mypage.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
 import java.util.List;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,5 +14,6 @@ public class NotificationPermissionsUpdateRequest {
     @Schema(
         type = "array",
         description = "알림 유형별 동의 여부")
+    @NotEmpty(message = "알림 유형별 동의 여부는 반드시 포함되어야 합니다.")
     List<NotificationPermissionDetail> permissions;
 }
