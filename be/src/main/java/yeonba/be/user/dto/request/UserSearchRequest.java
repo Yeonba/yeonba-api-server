e03f107b182f30031a6c8056b562db573a019814
Minjae-An <mj3242@naver.com>
2024-05-18 21:16:26 +0900

    feat: 이성 검색 요청 DTO, request body에서 매핑하도록 변경(#54)

--- be/src/main/java/yeonba/be/user/dto/request/UserSearchRequest.java
+++ be/src/main/java/yeonba/be/user/dto/request/UserSearchRequest.java
@@ -1,74 +1,65 @@
 package yeonba.be.user.dto.request;
 
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.PositiveOrZero;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Range;
 
 @Getter
-@AllArgsConstructor
+@NoArgsConstructor
 public class UserSearchRequest {
 
-    @Parameter(
-        name = "page",
+    @Schema(
+        type = "number",
         description = "페이지 번호, 기본 첫 페이지(0)",
-        example = "0",
-        in = ParameterIn.QUERY)
+        example = "0")
     @PositiveOrZero(message = "페이지 번호는 0이상이어야 합니다.")
     private Integer page;
 
-    @Parameter(
-        name = "area",
-        description = "활동 지역",
-        example = "서울",
-        in = ParameterIn.QUERY)
+    @Schema(
+        type = "string",
+        description = "검색 지역",
+        example = "서울")
     private String area;
 
-    @Parameter(
-        name = "vocalRange",
-        description = "음역대",
-        example = "고음",
-        in = ParameterIn.QUERY)
+    @Schema(
+        type = "string",
+        description = "검색 음역대",
+        example = "고음")
     private String vocalRange;
 
-    @Parameter(
-        name = "ageLowerBound",
+    @Schema(
+        type = "number",
         description = "검색 나이 하한",
-        example = "20",
-        in = ParameterIn.QUERY)
+        example = "20")
     @Range(min = 20, max = 40, message = "검색 나이는 20~40내 값만 가능합니다.")
     private Integer ageLowerBound;
 
-    @Parameter(
-        name = "ageUpperBound",
+    @Schema(
+        type = "number",
         description = "검색 나이 상한",
-        example = "40",
-        in = ParameterIn.QUERY)
+        example = "30")
     @Range(min = 20, max = 40, message = "검색 나이는 20~40내 값만 가능합니다.")
     private Integer ageUpperBound;
 
-    @Parameter(
-        name = "heightLowerBound",
+    @Schema(
+        type = "number",
         description = "검색 키 하한",
-        example = "150",
-        in = ParameterIn.QUERY)
+        example = "160")
     @Range(min = 130, max = 220, message = "검색 키는 130~220cm 내 값만 가능합니다.")
     private Integer heightLowerBound;
 
-    @Parameter(
-        name = "heightUpperBound",
+    @Schema(
+        type = "number",
         description = "검색 키 상한",
-        example = "180",
-        in = ParameterIn.QUERY)
+        example = "170")
     @Range(min = 130, max = 220, message = "검색 키는 130~220cm 내 값만 가능합니다.")
     private Integer heightUpperBound;
 
-    @Parameter(
-        name = "includePreferredAnimal",
-        description = "검색 기준에 선호하는 동물상 포함",
-        example = "true",
-        in = ParameterIn.QUERY)
+    @Schema(
+        type = "boolean",
+        description = "검색 기준에 선호하는 동물상 포함 여부",
+        example = "true")
     private Boolean includePreferredAnimal;
 }
