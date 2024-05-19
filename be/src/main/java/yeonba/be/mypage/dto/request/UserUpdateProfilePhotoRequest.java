874ca85c991582738eeac2b5dbf7cc2daa04a0b6
Minjae-An <mj3242@naver.com>
2024-05-19 20:19:10 +0900

    feat: 사진 싱크로율 필드 추가(#90)

--- be/src/main/java/yeonba/be/mypage/dto/request/UserUpdateProfilePhotoRequest.java
+++ be/src/main/java/yeonba/be/mypage/dto/request/UserUpdateProfilePhotoRequest.java
@@ -1,6 +1,7 @@
 package yeonba.be.mypage.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.util.List;
@@ -18,4 +19,12 @@ public class UserUpdateProfilePhotoRequest {
     @Size(min = 2, max = 2, message = "프로필 사진은 정확히 2장이어야 합니다.")
     @NotNull(message = "프로필 사진은 반드시 입력되어야 합니다.")
     List<MultipartFile> profilePhotos;
+
+    @Schema(
+        type = "number",
+        description = "사진 싱크로율",
+        example = "85")
+    @Min(value = 75, message = "사진 싱크로율이 75퍼이상이어야 합니다.")
+    @NotNull(message = "사진 싱크로율은 반드시 입력되어야 합니다.")
+    private int photoSyncRate;
 }
