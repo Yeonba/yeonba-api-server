0b53e82b2f4c088cd3926fdfbac5ec578e7bee95
kim seonggyu <71162390+acceptor-gyu@users.noreply.github.com>
2024-05-25 21:23:42 +0900

    인터셉터에서 request jwt를 활용하도록 수정 (#95)
    * fix: 메서드명 수정(#94)
    
    * feat: default request user to jwt(#94)
    
    * feat: 스웨거에서 api 호출 시 jwt를 요구하도록 수정(#94)
    
    * fix: 불필요한 검증 로직 제거(#94)

--- be/src/main/java/yeonba/be/config/SwaggerConfig.java
+++ be/src/main/java/yeonba/be/config/SwaggerConfig.java
@@ -3,6 +3,8 @@ package yeonba.be.config;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,14 +18,23 @@ public class SwaggerConfig {
         Info info = new Info()
             .title("Yeonba API Docs")
             .description("연바 API 명세서입니다.")
-            .description("<h3> Yeonba API</h3><img src=\"https://avatars.githubusercontent.com/u/156646513?s=200&v=4\" width=150 height=150 />")
+            .description(
+                "<h3> Yeonba API</h3><img src=\"https://avatars.githubusercontent.com/u/156646513?s=200&v=4\" width=150 height=150 />")
             .version("0.0.1");
 
         return new OpenAPI()
-            .components(new Components())
-//            .addServersItem(new Server().url("https://api.yeonba.co.kr"))
+            .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+            .components(
+                new Components().addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()))
+            .addServersItem(new Server().url("https://api.yeonba.co.kr"))
             .info(info);
     }
 
+    private SecurityScheme createAPIKeyScheme() {
 
+        return new SecurityScheme()
+            .type(SecurityScheme.Type.HTTP)
+            .bearerFormat("JWT")
+            .scheme("bearer");
+    }
 }
