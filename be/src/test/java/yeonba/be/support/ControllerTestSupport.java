473d20818ceff39264a710fe23c023cc3c4558cd
Minjae-An <mj3242@naver.com>
2024-05-25 21:47:06 +0900

    feat: controller 테스트 공통 환경 추가(#89)
    임의의 controller를 설정하여 테스트할 수 있도록 구성

---
+++ be/src/test/java/yeonba/be/support/ControllerTestSupport.java
@@ -0,0 +1,32 @@
+package yeonba.be.support;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import yeonba.be.config.DevAuthInterceptor;
+import yeonba.be.exception.ExceptionAdvice;
+
+@ActiveProfiles("test")
+public abstract class ControllerTestSupport {
+
+    protected MockMvc mockMvc;
+    protected ObjectMapper objectMapper = new ObjectMapper();
+
+    @MockBean
+    protected DevAuthInterceptor devAuthInterceptor;
+
+    @BeforeEach
+    void setUp() {
+        this.mockMvc = MockMvcBuilders.standaloneSetup(controller())
+            .setControllerAdvice(new ExceptionAdvice())
+            .alwaysDo(print())
+            .build();
+    }
+
+    protected abstract Object controller();
+}
