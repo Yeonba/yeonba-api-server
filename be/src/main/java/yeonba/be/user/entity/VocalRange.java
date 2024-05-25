313b5ad9da674811215b7a338eeb25f501eee6fd
Minjae-An <mj3242@naver.com>
2024-05-25 21:32:42 +0900

    feat: PK 설정 추가(#89)

--- be/src/main/java/yeonba/be/user/entity/VocalRange.java
+++ be/src/main/java/yeonba/be/user/entity/VocalRange.java
@@ -4,6 +4,7 @@ package yeonba.be.user.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
@@ -17,7 +18,7 @@ import org.apache.commons.lang3.StringUtils;
 public class VocalRange {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
