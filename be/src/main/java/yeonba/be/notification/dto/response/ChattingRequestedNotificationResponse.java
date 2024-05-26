35f56210f9b049ae2fe8eca445fa0ea953567f9a
Minjae-An <mj3242@naver.com>
2024-05-26 21:56:57 +0900

    feat: 채팅 요청 알림 응답 DTO 추가(#98)
    채팅 요청 알림의 경우 채팅을 요청한 사용자와의 채팅 가능 여부 포함

---
+++ be/src/main/java/yeonba/be/notification/dto/response/ChattingRequestedNotificationResponse.java
@@ -0,0 +1,49 @@
+package yeonba.be.notification.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import yeonba.be.notification.entity.Notification;
+import yeonba.be.user.entity.User;
+
+@Getter
+public class ChattingRequestedNotificationResponse extends NotificationResponse {
+
+    @Schema(
+        type = "boolean",
+        description = "채팅 가능 여부",
+        example = "false")
+    @JsonProperty("canChat")
+    private boolean canChat;
+
+    public ChattingRequestedNotificationResponse(
+        String notificationType,
+        String content,
+        long senderId,
+        String senderProfilePhotoUrl,
+        String senderNickname,
+        LocalDateTime createdAt,
+        boolean canChat) {
+
+        super(notificationType, content, senderId, senderProfilePhotoUrl, senderNickname,
+            createdAt);
+        this.canChat = canChat;
+    }
+
+    public static ChattingRequestedNotificationResponse from(Notification notification,
+        boolean chattingPossible) {
+
+        User sender = notification.getSender();
+
+        return new ChattingRequestedNotificationResponse(
+            notification.getType().name(),
+            notification.getContent(),
+            sender.getId(),
+            sender.getRepresentativeProfilePhoto(),
+            sender.getNickname(),
+            notification.getCreatedAt(),
+            chattingPossible
+        );
+    }
+}
