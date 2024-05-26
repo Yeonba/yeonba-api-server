1d64320796531b7d513c5c87d4927bc662499889
Minjae-An <mj3242@naver.com>
2024-05-26 21:53:25 +0900

    feat: 알림 응답 DTO, 추상 클래스로 전환(#98)
    - 알림 응답에 공통으로 포함되는 데이터 정의
    - 해당 클래스를 상속 받아 부가적인 데이터를 포함한 알림 응답 DTO 구현

--- be/src/main/java/yeonba/be/notification/dto/response/NotificationResponse.java
+++ be/src/main/java/yeonba/be/notification/dto/response/NotificationResponse.java
@@ -4,20 +4,18 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import yeonba.be.notification.entity.Notification;
-import yeonba.be.user.entity.User;
 
 @Getter
 @AllArgsConstructor
-public class NotificationResponse {
+public abstract class NotificationResponse {
 
     @Schema(
         type = "string",
         description = """
             알림 타입, 다음 종류 존재
             - ARROW_RECEIVED (화살 수신)
-            - CHAT_REQUESTED (채팅 요청 수신)
-            - CHAT_REQUEST_ACCEPTED (요청한 채팅 수락됨)""",
+            - CHATTING_REQUESTED (채팅 요청 수신)
+            - CHATTING_REQUEST_ACCEPTED (요청한 채팅 수락됨)""",
         example = "ARROW_RECEIVED")
     private String notificationType;
 
@@ -50,18 +48,4 @@ public class NotificationResponse {
         description = "알림 생성 일시",
         example = "2024-04-10 10:12:00.112233")
     private LocalDateTime createdAt;
-
-    public static NotificationResponse from(Notification notification) {
-
-        User sender = notification.getSender();
-
-        return new NotificationResponse(
-            notification.getType().name(),
-            notification.getContent(),
-            sender.getId(),
-            sender.getRepresentativeProfilePhoto(),
-            sender.getNickname(),
-            notification.getCreatedAt()
-        );
-    }
 }
